fbab03017fe27b0ce9570fa8a4a08e503306895e
torsten_hoeltge <torsten_hoeltge@6c400f8c-763c-0410-ad1e-de11c01e913c>
2009-08-23 07:47:31 +0000

    corrected version number in version.php

--- core/includes/version.php
+++ core/includes/version.php
@@ -1,5 +1,8 @@
 <?php
-$kga['version']  = "0.8.0";
-$kga['revision'] = 859;
+$kga['version']  = "0.8.1";
+// $kga['revision'] is the sourceforge SVN revision.
+$kga['revision'] = 0005;
+// when Kimai moved from syncom to sourceforge we had r866 ...
+$kga['revision'] = $kga['revision'] += 866;
 $kga['status']   = "BETA"; // leave blank if stable ...
 ?>
