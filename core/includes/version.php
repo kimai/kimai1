f4e3e2dace32e8e9bf92a28ba7f6bfe523d4dd2b
torsten_hoeltge <torsten_hoeltge@6c400f8c-763c-0410-ad1e-de11c01e913c>
2009-10-04 09:11:27 +0000

    xp_ext: css tweaks ...

--- core/includes/version.php
+++ core/includes/version.php
@@ -1,7 +1,7 @@
 <?php
 $kga['version']  = "0.8.2";
 // $kga['revision'] is the sourceforge SVN revision.
-$kga['revision'] = '0109';
+$kga['revision'] = '0110';
 // when Kimai moved from syncom to sourceforge we had r866 ...
 $kga['revision'] = (int)$kga['revision'] += 866;
 $kga['status']   = "BETA"; // leave blank if stable ...
