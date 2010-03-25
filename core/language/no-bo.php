97a35984796c9843bde5ccc5f43cc1b05857aa88
ServiusHack <ServiusHack@6c400f8c-763c-0410-ad1e-de11c01e913c>
2010-03-25 13:31:19 +0000

    Added norwegian language file by Per Ulrich Kristiansen

---
+++ core/language/no-bo.php
@@ -0,0 +1,342 @@
+<?php
+/**
+ * This file is part of 
+ * Kimai - Open Source Time Tracking // http://www.kimai.org
+ * (c) 2006-2009 Kimai-Development-Team
+ * 
+ * Kimai is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; Version 3, 29 June 2007
+ * 
+ * Kimai is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Kimai; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ */
+ 
+/**
+ * norwegian bokmål
+ */
+ 
+ /**
+ * Please report translation errors and inaccuracies to TBT@TBT.no
+  */
+ /**
+ * Translated by Per Ulrich Kristiansen TBT@TBT.no (+47) 48 14 92 14
+ * http://naturTIPS.no - http://TBT.no
+ * Tekst Bilde Trykksak AS, 9925 Svanvik, Norway
+ * This translation contains errors and inaccuracies.
+ * Use it on your own risk.
+ * We take no responsibility for any consequence.
+ */
+$kga['lang']= array(
+"countryCode"       => "no",
+"preferences"       => "Innstillinger",
+"total"		        => "Totalt",
+"monatzeit"         => "Måned",
+"heute"             => "i dag",
+"gestern"           => "i går",
+"autoOn"            => "Automatisk på",
+"autoOff"           => "Automatisk av",
+"datum"             => "Dato",
+"in"                => "Inn",
+"out"               => "Ut",
+"time"              => "t'm",
+"timelabel"         => "Tid",
+"durationlabel"     => "Duration",
+"knd"               => "Kunde",
+"pct"               => "Prosjekt",
+"evt"               => "Oppgave",
+"knds"              => "Kunder",
+"pcts"              => "Prosjekter",
+"evts"              => "Oppgaver",
+"admin"             => "Admin",
+"rowlimit"          => "Maks. rader",
+"breiteZeitspalte"  => "bredde tid kolonne",
+"breiteUhrspalte"   => "bredde klokke kolonne",
+"breiteDatumspalte" => "bredde dato kolonne",
+"def_pctID"         => "standard prosjekt ID",
+"def_kndID"         => "standard kunde ID",
+"def_evtID"         => "standard hendelse ID",
+"recommended"       => "anbefales",
+"submit"            => "Send",
+"cancel"            => "Avbryt",
+"edit"              => "Rediger oppføring",
+"construction"      => "under oppføring...",
+"about"             => "Om",
+"hours"             => "timer",
+"mins"              => "minutter",
+"secs"              => "sekunder",
+"skin"              => "Design",
+"erase"             => "Slett alt",
+"new_knd"           => "Ny kunde",
+"new_pct"           => "Nytt prosjekt",
+"new_evt"           => "Ny oppgave",
+"day_summary"       => "Total tid",
+"plzChoose"         => "Velg her",
+"noEntries"         => "Finner ikke søkeresultat for den angitt tidsperioden...",
+"recordAgain"       => "Registrer denne oppgaven på nytt",
+"comment"           => "Kommentar",
+"zlocation"         => "Sted",
+"trackingnr"        => "Sporingsnummer",
+"cookiewarning"     => "Slå på informasjons kapsler i nettleseren før du fortsetter.",
+"JSwarning"         => "JavaScript må være aktivert i nettleseren.",
+"designation"       => "betegnelse",
+"banned"            => "Utestengt (Kontoen er låst)",
+"tooManyLogins"     => "For mange mislykkede påloggingsforsøk - kontoen er utestengt.",
+"accessDenied"      => "Sperret tilgang!",
+"wrongPass"         => "Feil brukernavn eller/og passord",
+"tryAgain"          => "Prøv igjen / Glemt passordet?",
+"back"              => "Tilbake til skjermbildet for pålogging",
+"bye"               => "Takk for nå!",
+"login"             => "Logg på",
+"logout"            => "Logg ut",
+"loggedon"          => "Logg på som bruker:",
+"username"          => "Brukernavn",
+"password"          => "Passord",
+"newPassword"       => "Nytt passord",
+"minLength"         => "min. 5 tegn!",
+"update"            => "Database strukturen på gjeldende Kimai versjon bør oppdateres.<br />Vi anbefaler at du sikkerhetskopierer ditt arbeid for å unngå å miste informasjon <br />under oppdateringsprosessen.",
+"updateNow"         => "Oppdater databasen nå!",
+"stop"              => "Stopp!",
+"start"             => "Start!",
+"setTimespace"      => "angi tiden",   
+"now"               => "nå",
+"specify"           => "angi liste",
+"add"               => "legg til",
+"running"           => "Kjører...",
+"company"           => "Selskap",
+"street"            => "Gate",
+"zipcode"           => "Postnummer",
+"city"              => "Sted",
+"telephon"          => "Telefon",
+"fax"               => "Faks",
+"mobilephone"       => "Mobil",
+"mail"              => "E-post",
+"homepage"          => "Internett",
+"users"             => "Brukere",
+"name"              => "Navn",
+"alias"             => "Alias",
+"adduser"           => "Legg til bruker",
+"addgroup"          => "Legg til gruppe",
+"group"             => "Gruppe",
+"groups"            => "Grupper",
+"advanced"          => "Avansert",
+"database"          => "Database",
+"options"           => "Alternativer",
+"status"            => "Status",
+"mode"              => "test modus = 0 / normal modus (brukere må logge på) = 1",
+"adminmail"         => "e-post adressen til administrator",
+"logintries"        => "antall mislykkede pålogginger før brukeren blir utestengt",
+"bantime"           => "sperret for adgang til å logge inn i sekunder",
+"lastdbbackup"      => "Nyeste sikkerhetskopi",
+"runbackup"         => "Ta sikkerhetskopi nå",
+"defaultgrp"        => "Ordinær (0)",
+"regusr"            => "Ordinær bruker",
+"leaderusr"         => "Førende bruker",
+"adminusr"          => "Admin bruker",
+"editusr"           => "Redigere bruker",
+"editgrp"           => "Redigere gruppe",
+"delgrp"            => "delete group",
+"delpct"            => "delete project",
+"delknd"            => "delete customer",
+"switchusr"         => "Bytt til denne brukeren",
+"backupusr"         => "Ta sikkerhetskopi av denne brukeren",
+"mailusr"           => "Send e-post til denne brukeren",
+"delusr"            => "Slett denne brukeren",
+"bannedusr"         => "Utestengt bruker - klikk for å aktivere på ny",
+"activeusr"         => "Aktiv bruker - klikk for å utestenge",
+"changegrp"         => "Endre gruppe",
+"members"           => "Medlem",
+"groupleader"       => "Gruppesjef",
+"nopassword"        => "ikke angitt passord",
+"noItems"           => "ingen oppføringer",
+"DBname"            => "Navn på database",
+"updated"           => "Oppføring(er) oppdatert!",
+"autoselection"     => "Automatisk valg",
+"quickdelete"       => "Rask sletting",
+"tools"             => "Verktøy",
+"print"             => "Utskrift",
+"save"              => "Lagre",
+"export"            => "Eksporter",
+"groupname"         => "Gruppe navn",
+"help"              => "Hjelp",
+"select"            => "Velg",
+"hidedeletedusers"  => "Skjul slettede brukere",
+"showdeletedusers"  => "Vis slettede brukere",
+
+"month"             => "Måned",
+"day"               => "Dag",
+"year"              => "År",
+
+"lang"              => "Språk",
+
+"ac"                => "Automatisk videreført oppføring.",
+"ac_error"          => "Automatisk videreført oppføring med feil (tidsangivelsen er for lang).",
+"comment_type"      => "Kommentar type",
+"ctype0"            => "Kommentar",
+"ctype1"            => "Notat",
+"ctype2"            => "Forsiktig!",
+"hint"              => "Tips:",
+"usr_caution1"      => "Dersom du endrer brukernavnet på",
+"usr_caution2"      => "du må logge inn på nytt med det nye brukernavnet med en gang!",
+"timediff_warn"     => "Du må angi en annen tidsperiode i feltene for tid inn og tid ut!",
+"visibility"        => "Skal vises",
+"showitem"          => "gjør synlig",
+"hideitem"          => "skjul",
+"checkUsername"     => "Du må skrive inn et brukernavn i feltet til venstre for legg-til knappen!",
+"clickToReload"     => "Klikk her for å gjenoppfriske brukergrensesnittet",
+"sure"              => "Sikker?",
+"clock"             => "Klokke",
+"currSum"           => "Sammendrag for gjeldende tidsperiode?",
+"admWarn"           => "Du er Admin. Status kan kun angis til lavere verdi av en annen Admin.",
+"flip_pct_display"  => "Sorter prosjekter etter kunde",
+"pct_comment_flag"  => "Vis alltid kommentarer ved siden av prosjekter",
+"showIDs"           => "Vis ID'er",
+"error"             => "Det har oppstått en feil!",
+"checkupdate"       => "Se etter oppdateringer",
+
+"selectR"           => "Kunde / Prosjekt / Oppgave:",
+"selectKND"         => "K:",
+"selectPCT"         => "P:",
+"selectEVT"         => "O:",
+
+"testKND"           => "Test-kunde",
+"testPCT"           => "Test-Prosjekt",
+"testEVT"           => "testing",
+
+"budget"            => "Budget",
+"rate"              => "Timepris",
+"rate_short"        => "Timepris",
+"default_rate"      => "Standard timepris",
+"my_rate"           => "Min timepris",
+
+"wage"              => "Beløp i kr",
+
+"customerlogin"     => "kunde innlogging",
+
+"expense"           => "Utgifter",
+"designation"       => "Betegnelse",
+
+"cleared"           => "Slett innhold",
+
+"noFading"          => "no fading effect",
+
+'show_sensible_data' => 'Display sensible data as well in the debug extension.',
+'show_update_warn' => 'Turn off warning before an update.',
+'check_at_startup' => 'Show a message on the login page if a newer version is available.',
+'show_daySeperatorLines' => 'Draw a line between entries of different days.',
+'show_gabBreaks' => 'Draw a line between two records that are not perfectly continuous.',
+'show_RecordAgain' => 'Show the &quot;Record Again&quot; button in front of every entry.',
+'show_TrackingNr' => 'Allows to edit the field Tracking-Nr.',
+'currency_name' => 'Name of the currency to use.',
+'currency_sign' => 'Sign of the currency to use.',
+);
+
+$kga['lang']['months']         = array("Januar","Februar","Mars","April","Mai","Juni","Juli","August","September","Oktober","November","Desember");
+$kga['lang']['months_short']   = array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Okt","Nov","Des");
+$kga['lang']['weekdays']       = array("Søndag","Mandag","Tirsdag","Onsdag","Torsdag","Fredag","Lørdag");
+$kga['lang']['weekdays_short'] = array("Søn","Man","Tir","Ons","Tor","Fre","Lør");
+$kga['lang']['errors'][0]['hdl'] = "Kunne ikke koble til databasen!";
+$kga['lang']['errors'][0]['txt'] = "Vær så snill å kontrollere at din database er aktivert, har en aktiv PDO tilkobling og at oppføringene i 'conf.php' i mappen 'includes' er korrekte.";
+
+$kga['lang']['errors'][1]['hdl'] = "Feil under installasjonen!";
+$kga['lang']['errors'][1]['txt'] = 'Kimai klarer ikke å opprette nødvendige oppføringer i databasen fordi gjeldende oppføringer med samme navn finnes fra før.<br/> For å oppdatere din gjeldende installasjon vær så snill å trykk  å <a href="../index.php">her</a>.<br/> Velg et annet fortegn til tabellene eller slett gjeldende tabeller med dette fortegnet slik at Kimai får anledning til å utføre en basis installasjon.';
+$kga['lang']['updater'][0]        = "Ja, jeg har en sikkerhetskopi av min Kimai database! Fortsett med oppdateringen!";
+$kga['lang']['updater'][10]       = "Tegnforklaring:";
+$kga['lang']['updater'][20]       = "Spørringen var vellykket.";
+$kga['lang']['updater'][30]       = "Det oppsto en feil under spørringen - mest trolig uviktig.";
+$kga['lang']['updater'][40]       = "Spørringen førte til en skjebnesvanger feil.";
+$kga['lang']['updater'][50]       = "Logg over sikkerhetskopi:";
+$kga['lang']['updater'][60]       = "Sikkerhetskopieringen mislyktes - Oppdateringen avbrutt!";
+$kga['lang']['updater'][70]       = "Oppdateringslogg:";
+$kga['lang']['updater'][80]       = "Oppdateringen av databasen var vellykket og sikkerhetskopi er tatt.";
+$kga['lang']['updater'][90]       = "Spørringer utført.";
+$kga['lang']['updater'][100]      = "Oppdateringen av databasen mislyktes. En sikkerhetskopi ble tatt. Denne kan brukes til å gjenskape databasen i fall Kimai ikke fungerer tilfredsstillende.";
+$kga['lang']['updater'][110]      = "Nye passord";
+$kga['lang']['updater'][120]      = "Se nedenfor om nye passord for den enkelte bruker.";
+$kga['lang']['updater'][130]      = "Kunne ikke skrive til filen autoconf.php.";
+$kga['lang']['updater'][140]      = "oppdaterer filen autoconf.php";
+
+$kga['lang']['backup'][0]        = "Dette verktøyet for sikkerhetskopierings- og gjenoppretting er ikke ferdig testet! Bruk det på eget ansvar!";
+$kga['lang']['backup'][1]        = "Tilgjengelige sikkerhetskopier:";
+$kga['lang']['backup'][2]        = "Gjenopprett";
+$kga['lang']['backup'][3]        = "Slett";
+$kga['lang']['backup'][4]        = "OK";
+$kga['lang']['backup'][5]        = "Du kan bare gjenopprette en sikkerhetskopi om gangen!";
+$kga['lang']['backup'][6]        = "Sikkerhetskopier fra";
+$kga['lang']['backup'][7]        = "ble gjenopprettet.";
+$kga['lang']['backup'][8]        = "opprett sikkerhetskopi";
+$kga['lang']['backup'][9]        = "Disse sikkerhetskopiene er egentlig øyeblikksbilder! Det vil si at de eksisterer som en del av samme database som Kimai benytter. Sikkerhetskopi med dette verktøyet er IKKE en erstatning for skikkelige sikkerhetskopier over på et annet fysisk medium. Det finnes rikelig med verktøy for sikkerhetskopiering av databaser på nettet;)";
+
+$kga['lang']['xp_ext'] = array(
+	
+"print_hint"      		   => "Trykk OK for å opprette en ny utskriftsvennlig side i tabellformat.",
+"dl_hint"         		   => "Trykk OK for å opprette og laste ned dokumentet.",
+                              
+"tab_filter"	 		   => "Filter",
+"tab_location"	 		   => "Stedangivelse",
+"tab_timeformat"	 	   => "Tidsformat",
+                              
+"cleared_all"	 		   => "Alle",
+"cleared_cleared" 		   => "Kun ledige",
+"cleared_open"	 		   => "Kun åpne",
+"stdrd_location"  		   => "Angi sted",
+"timeformat"	     	   => "Tidsformat",
+"dateformat"			   => "Datoformat",
+                              
+"print_comment"   		   => "Skriv ut kommentarer",
+"print_summary"   		   => "Skriv ut sammendrag",
+"create_bookmarks"		   => "Lag bokmerker",
+"download_pdf"    		   => "Last ned som PDF",
+"pdf_format"             => "Format",
+"export_pdf"             => "tabell",
+"export_pdf2"            => "liste",
+                              
+"column_delimiter"		   => "Skilletegn for kolonner",
+"quote_char"      		   => "Skilletegn for felt",
+                              
+"exportCSV"         	   => "CSV eksport",
+"exportPDF"         	   => "PDF eksport",
+"exportXLS"         	   => "Excel eksport",
+"print"         	       => "Utskrift",
+"export_timeformat_help"   => "Hjelp med tidsformat",
+
+'subtotal'              => 'Del sum',
+'finalamount'           => 'Total',
+'pdf_headline'          => 'Liste over arbeidstid og utgifter',
+'costs'                 => 'Utgifter',
+'expense'               => 'Kostnader',
+'summary'               => 'Sammendrag',
+'page'                  => 'Side',
+'by'                    => 'av',
+'done_by'               => 'utført av',
+'duration'              => 'Varighet',
+'hide_cleared_entries'  => 'don\'t show cleared entries',
+'customer_new_page'     => 'new page for each customer',
+
+'decimal_separator'  => 'Decimal separator',
+
+);
+
+
+
+$kga['lang']['ext_invoice'] = array(
+	
+"invoiceTitle" 			=> "Lag faktura",
+"invoiceProject" 		=> "Prosjekter:",
+"invoiceTimePeriod" 	=> "Tidsperiode:",
+"invoiceOptionVat" 		=> " Faktura med MVA",
+"invoiceOptionShort" 	=> "Kort faktura",
+"invoiceOptionRound" 	=> "Rundetid",
+"invoiceButton" 		=> "Lag"
+
+);
+
+
+?>
