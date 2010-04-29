1d7642cc9de06441b4f3a638647c91d4765439e2
ServiusHack <ServiusHack@6c400f8c-763c-0410-ad1e-de11c01e913c>
2010-04-29 17:16:49 +0000

    Setting several no-cache options at some more places to prevent IE from caching.

--- core/extensions/ki_timesheets/init.php
+++ core/extensions/ki_timesheets/init.php
@@ -22,7 +22,11 @@ $tpl->compile_dir  = 'compile/';
 
 $tpl->assign('kga', $kga);
 
-
+// prevent IE from caching the response
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 
 // @Oleg: ich brauche bitte ein Array:
