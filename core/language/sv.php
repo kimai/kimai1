45e566106e2057d11d0230ff08c1ad1e4d29f7af
ServiusHack <ServiusHack@6c400f8c-763c-0410-ad1e-de11c01e913c>
2010-05-10 22:15:12 +0000

    Added Swedish language file by Conny Sjölund.

---
+++ core/language/sv.php
@@ -0,0 +1,357 @@
+<?php
+/**
+ * This file is part of 
+ * Kimai - Open Source Time Tracking // http://www.kimai.org
+ * (c) 2006-2009 Kimai-Development-Team
+ * 
+ * Kimai is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; Version 3, 29 June 2007
+ * 
+ * Kimai is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Kimai; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ */
+ 
+/**
+ * Swedish - Svenska
+ * Based on Kimai english version 0.9.1.1138
+ * By Conny Sjölund
+ * We take no responsibility for any consequence.
+ */
+$kga['lang']= array(
+"countryCode"       => "sv-SV",
+"preferences"       => "Inställningar",
+"total"		    => "Totalt",
+"monatzeit"         => "Månad",
+"heute"             => "idag",
+"gestern"           => "igår",
+"autoOn"            => "Auto på",
+"autoOff"           => "Auto av",
+"datum"             => "Datum",
+"in"                => "In",
+"out"               => "Ut",
+"time"              => "t:m",
+"timelabel"         => "Tid",
+"durationlabel"     => "Varaktighet",
+"knd"               => "Kund",
+"pct"               => "Projekt",
+"evt"               => "Uppgift",
+"knds"              => "Kunder",
+"pcts"              => "Projekt",
+"evts"              => "Uppgifter",
+"admin"             => "Admin",
+"rowlimit"          => "max. rader",
+"breiteZeitspalte"  => "width time column",
+"breiteUhrspalte"   => "width clock column",
+"breiteDatumspalte" => "width date column",
+"def_pctID"         => "standard projekt ID",
+"def_kndID"         => "standard kund ID",
+"def_evtID"         => "standard händelse ID",
+"recommended"       => "rekommenderad",
+"submit"            => "OK",
+"cancel"            => "Avbryt",
+"edit"              => "Redigera Post",
+"construction"      => "under konstruktion...",
+"about"             => "Om",
+"hours"             => "timmar",
+"mins"              => "minuter",
+"secs"              => "sekunder",
+"skin"              => "Gränssnitt (skin)",
+"erase"             => "Radera Post",
+"new_knd"           => "lägg till kund",
+"new_pct"           => "lägg till projekt",
+"new_evt"           => "lägg till uppgift",
+"day_summary"       => "Tidsummering",
+"plzChoose"         => "Var god välj",
+"noEntries"         => "Inga registrerade data för den valda tidsperioden...",
+"recordAgain"       => "Registrera denna händelse igen",
+"comment"           => "Kommentar",
+"zlocation"         => "Plats",
+"trackingnr"        => "Register Nummer",
+"cookiewarning"     => "Aktivera kakor (Cookies) innan du fortsätter.",
+"JSwarning"         => "JavaScript måste vara aktiverat.",
+"designation"       => "beteckning",
+"banned"            => "Banned (Kontot låst)",
+"tooManyLogins"     => "För många felaktiga inloggningar - användaren frånkopplad.",
+"accessDenied"      => "Nekat tillträde!",
+"wrongPass"         => "Fel Anvandarnamn och/eller Lösenord",
+"tryAgain"          => "Försök igen / glömt lösenord",
+"back"              => "Tillbaka till inloggningsfönstret",
+"bye"               => "Hej då!",
+"login"             => "Logga in",
+"logout"            => "Logga ut",
+"loggedon"          => "Inloggad som användare:",
+"username"          => "Användarnamn",
+"password"          => "Lösenord",
+"newPassword"       => "Nytt Lösenord",
+"minLength"         => "minst 5 tecken!",
+"update"            => "Databasstrukturen för den Installerade Kimai versionen måste uppdateras.<br />Vi rekommenderar att säkerhetskopia görs på registeruppgifterna så att ingen data går förlorad<br />under processen.",
+"updateNow"         => "Uppdatera databasen nu!",
+"stop"              => "Stopp!",
+"start"             => "Start!",
+"setTimespace"      => "Ange tidsperiod",   
+"now"               => "nu",
+"specify"           => "Ange lista",
+"add"               => "lägg till",
+"running"           => "Kör...",
+"company"           => "Företag",
+"street"            => "Gata",
+"zipcode"           => "Postkod",
+"city"              => "Stad",
+"telephon"          => "Telefon",
+"fax"               => "Fax",
+"mobilephone"       => "Mobiltelefon",
+"mail"              => "Mejl",
+"homepage"          => "Internet",
+"users"             => "Användare",
+"name"              => "Namn",
+"alias"             => "Smeknamn",
+"adduser"           => "Lägg till användare",
+"addgroup"          => "Lägg till grupp",
+"group"             => "Grupp",
+"groups"            => "Grupper",
+"advanced"          => "Avancerad",
+"database"          => "Databas",
+"options"           => "Alternativ",
+"status"            => "Status",
+"mode"              => "demonstrations läge = 0 / normalt läge (användare måste logga in) = 1",
+"adminmail"         => "mejl adress till administratören",
+"logintries"        => "antal felaktiga inloggningar inan användaren blir bannlyst",
+"bantime"           => "Bannlyst inloggning i sekunder",
+"lastdbbackup"      => "Senaste säkerhetskopia",
+"runbackup"         => "Säkerhetskopiera nu",
+"defaultgrp"        => "Standard (0)",
+"regusr"            => "Normal användare",
+"leaderusr"         => "Ledar användare",
+"adminusr"          => "Admin användare",
+"editusr"           => "Redigera användare",
+"editgrp"           => "Redigera grupp",
+"delgrp"            => "radera grupp",
+"delpct"            => "radera projekt",
+"delknd"            => "radera kund",
+"delevt"            => "radera uppgift",
+"switchusr"         => "Växla till denna användare",
+"backupusr"         => "Skapa en backup av denna användare",
+"mailusr"           => "Skicka ett mejl till denna användare",
+"delusr"            => "Radera denna användare",
+"bannedusr"         => "Bannlyst användare - klicka för att återaktivera",
+"activeusr"         => "Aktiv användare - klicka för att bannlysa",
+"changegrp"         => "ändra grupp",
+"members"           => "Medlemmar",
+"groupleader"       => "Grupp ledare",
+"nopassword"        => "Inget lösenord angivet",
+"noItems"           => "Inga Poster",
+"DBname"            => "Namn på databas",
+"updated"           => "Uppgift(er) uppdaterad!",
+"autoselection"     => "Autoval",
+"quickdelete"       => "Snabb-Radera",
+"tools"             => "Verktyg",
+"print"             => "Skriv ut",
+"save"              => "Spara",
+"export"            => "Exportera",
+"groupname"         => "Grupp namn",
+"help"              => "Hjälp",
+"select"            => "Välj",
+"hidedeletedusers"  => "Göm valda användare",
+"showdeletedusers"  => "Visa raderade användare",
+
+"month"             => "Månad",
+"day"               => "Dag",
+"year"              => "År",
+
+"lang"              => "Språk",
+
+"ac"                => "Automatically continued entry.",
+"ac_error"          => "Automatically continued entry with error (timespan too long).",
+"comment_type"      => "Kommentarens typ",
+"ctype0"            => "Kommentar",
+"ctype1"            => "Notering",
+"ctype2"            => "Varning!",
+"hint"              => "Tips:",
+"usr_caution1"      => "Om du ändrar användarnamn på",
+"usr_caution2"      => "du måste logga in på nytt med ditt nya användarnamn omedelbart!",
+"timediff_warn"     => "Du måste ange olika tider i fälten in/ut!",
+"visibility"        => "Synlighet",
+"showitem"          => "gör synlig",
+"hideitem"          => "gör osynlig",
+"checkUsername"     => "Du måste skriva in ett användarnamn i fältet till vänster om lägg till knappen!",
+"clickToReload"     => "Klicka här för att ladda om GUI",
+"sure"              => "Säker?",
+"clock"             => "Klocka",
+"currSum"           => "Sammanfattning av aktuell tidsperiod",
+"admWarn"           => "Du är Admin. Status kan endast sättas lägre av annan Admin.",
+"flip_pct_display"  => "Ordna projekttabell enligt kunder",
+"pct_comment_flag"  => "Visa alltid kommentarer bredvid projekt",
+"showIDs"           => "Visa ID:n",
+"error"             => "Ett fel har uppstått!",
+"checkupdate"       => "Leta efter uppdatering",
+
+"selectR"           => "Valt för registrering:",
+"selectKND"         => "K:",
+"selectPCT"         => "P:",
+"selectEVT"         => "T:",
+
+"testKND"           => "Test-Kund",
+"testPCT"           => "Test-Projekt",
+"testEVT"           => "testar",
+
+"budget"            => "Budget",
+"rate"              => "Pris (per timme)",
+"rate_short"        => "Pris",
+"default_rate"      => "Standard pris",
+"my_rate"           => "Mitt pris",
+
+"wage"              => "Arvode",
+
+"customerlogin"     => "kundinloggning",
+
+"expense"           => "Utgift",
+"designation"       => "Beteckning",
+
+"cleared"           => "Kvitterad",
+
+"noFading"          => "ingen fading effekt",
+"user_list_hidden"  => "hide user list by default",
+
+'show_sensible_data' 	=> 'Visa även känslig data i felsöknings (debug) tillägget.',
+'show_update_warn' 	=> 'Stäng av varning innan en uppdatering.',
+'check_at_startup' 	=> 'Visa ett meddelande på inloggningssidan om en nyare version finns tillgänglig.',
+'show_daySeperatorLines' => 'Rita en linje mellan poster i olika dagar.',
+'show_gabBreaks' 	=> 'Rita en linje mellan två registeruppgifter som inte är helt kontinuerliga.',
+'show_RecordAgain' 	=> 'Visar &quot;Record Again&quot; knappalternativ före varje post.',
+'show_TrackingNr' 	=> 'Tillåter att fältet Tracking-Nr redigeras.',
+'currency_name' 	=> 'Namn på valuta.',
+'currency_sign' 	=> 'Symbol för valuta.',
+'currency_first' => 'Show currency name in front of value (e.g. &euro; 100,00)',
+'display_date_format' => 'Date format of the timespan (<a href="http://php.net/manual/en/function.strftime.php" target="_blank">notation for strftime()</a>)',
+'display_currentDate_format' => 'Date format for the field below the timespan (<a href="http://php.net/manual/en/function.strftime.php" target="_blank">notation for strftime()</a>)',
+'table_date_format' => 'Date format for tables (<a href="http://php.net/manual/en/function.strftime.php" target="_blank">notation for strftime()</a>)',
+'round_time' => 'Round time to ',
+'round_time_minute' => ' minute(s).',
+'logged_in_as' => 'logged in as:',
+);
+
+$kga['lang']['months']         = array("January","February","Mars","April","May","Juni","Juli","Augusti","September","Oktober","November","December");
+$kga['lang']['months_short']   = array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Okt","Nov","Dec");
+$kga['lang']['weekdays']       = array("Söndag","Måndag","Tisdag","Onsdag","Torsdag","Fredag","Lördag");
+$kga['lang']['weekdays_short'] = array("Sön","Mon","Tis","Ons","Tor","Fri","Lör");
+
+$kga['lang']['errors'][0]['hdl'] = "Kunde inte koppla till databasen!";
+$kga['lang']['errors'][0]['txt'] = "Vänligen kontrollera att din databas är aktiverad, har du aktiv PDO kontakt och är uppgifterna i 'conf.php' i mappen 'includes' �r korrekta.";
+
+$kga['lang']['errors'][1]['hdl'] = "Fel vid installation!";
+$kga['lang']['errors'][1]['txt'] = 'Kimai kan inte skapa tabell eftersom namn med tabellen redan existerar.<br/>
+To update your existing installation please click <a href="../index.php">here</a>.<br/>
+Choose another table prefix or delete existing tables with this prefix in order to allow Kimai to perform a clean installation.';
+
+$kga['lang']['updater'][0]        = "Ja, jag har en säkerhetskopia av min Kimai databas! Fortsätt uppdateringen!";
+$kga['lang']['updater'][10]       = "Förklaring:";
+$kga['lang']['updater'][20]       = "Förfrågan utfördes framgångsrikt.";
+$kga['lang']['updater'][30]       = "Förfrågan orsakade fel - sannolikt irrelevant.";
+$kga['lang']['updater'][40]       = "Förfrågan orsakade allvarligt fel.";
+$kga['lang']['updater'][50]       = "Säkerhetskopiera Logg:";
+$kga['lang']['updater'][60]       = "Säkerhetskopia misslyckades - Uppdatering avbruten!";
+$kga['lang']['updater'][70]       = "Uppdaterings logg:";
+$kga['lang']['updater'][80]       = "Databasen uppdaterades med lyckat resultat och en säkerhetskopia skapades.";
+$kga['lang']['updater'][90]       = "Förfrågan processad.";
+$kga['lang']['updater'][100]      = "Uppdateringen av databasen misslyckades pga allvarligt fel. En säkerhetskopia skapades som kan anv�ndas f�r att �terskapa databasen ifall Kimai inte fungerar korrekt.";
+$kga['lang']['updater'][110]      = "Nya Lösenord";
+$kga['lang']['updater'][120]      = "Se nedan för nya lösenord för varje användare.";
+$kga['lang']['updater'][130]      = "Kunde inte skriva till autoconf.php.";
+$kga['lang']['updater'][140]      = "uppdatera filen autoconf.php";
+
+$kga['lang']['backup'][0]        = "Detta säkerhetskopieringsverktyg är inte ännu testat! Används på egen risk!";
+$kga['lang']['backup'][1]        = "Tillgängliga säkerhetskopior:";
+$kga['lang']['backup'][2]        = "återställ";
+$kga['lang']['backup'][3]        = "radera";
+$kga['lang']['backup'][4]        = "OK";
+$kga['lang']['backup'][5]        = "Du kan endast återställa *en* säkerhetskopia åt gången!";
+$kga['lang']['backup'][6]        = "Säkerhetskopia från";
+$kga['lang']['backup'][7]        = "blev återställd.";
+$kga['lang']['backup'][8]        = "skapa säkerhetskopia";
+$kga['lang']['backup'][9]        = "Dessa säkerhetskopior är egentligen ögonblicksbilder! Vilket innebär att de fortfarande finns i samma databas som din egentliga arbetsdata för Kimai. Säkerhetskopior via detta verktyg ersätter INTE verkliga fyskiska säkerhetskopior pä skild media. Det finns flera verktyg för säkerhetskopiering av databaser på internet ;)";
+
+
+$kga['lang']['xp_ext'] = array(
+	
+"print_hint"      	 => "Klicka OK för att öppna en skrivbar tabell.",
+"dl_hint"         	 => "Klikcka OK för att ladda ner en exporterad fil.",
+                              
+"tab_filter"	 	 => "Filter",
+"tab_location"	 	 => "Plats",
+"tab_timeformat"	 => "Tidsformat",
+                              
+"cleared_all"	 	 => "alla",
+"cleared_cleared" 	 => "endast kvitterade",
+"cleared_open"	 	 => "endast öppna",
+"stdrd_location"  	 => "Standard-Plats",
+"timeformat"	     	 => "Tidsformat",
+"dateformat"		 => "Datumformat",
+                             
+"print_comment"   	 => "skriv kommentarer",
+"print_summary"   	 => "skriv sammandrag",
+"create_bookmarks"	 => "skapa bokmärken",
+"download_pdf"    	 => "ladda ner som PDF",
+"pdf_format"             => "format",
+"export_pdf"             => "tabell",
+"export_pdf2"            => "lista",
+                              
+"column_delimiter"	 => "avgränsare för kolumner",
+"quote_char"      	 => "avgränsartecken för fält",
+                              
+"exportCSV"         	   => "CSV Export",
+"exportPDF"         	   => "PDF Export",
+"exportXLS"         	   => "XLS Export",
+"print"         	   => "Skriv ut",
+"export_timeformat_help"   => "Tidsformat hjälp",
+
+'subtotal'              => 'Delsumma',
+'finalamount'           => 'Totalt',
+'pdf_headline'          => 'Lista på arbetstid och utlägg',
+'costs'                 => 'Kostnad',
+'expense'               => 'Utlägg',
+'summary'               => 'Sammandrag',
+'page'                  => 'Sida',
+'by'                    => 'av',
+'done_by'               => 'utförd av',
+'duration'              => 'Varaktighet',
+'hide_cleared_entries'  => 'visa inte markerade poster',
+'customer_new_page'     => 'ny sida för varje kund',
+
+'decimal_separator'  	=> 'Decimal separerare',
+ 
+'times_and_expenses' => 'times and expenses',
+'times' => 'times',
+'expenses' => 'expenses',
+
+'reverse_order' => 'older entries first',
+'time_period'  => 'time period',
+
+);
+
+
+
+$kga['lang']['ext_invoice'] = array(
+	
+"invoiceTitle" 		=> "Create invoice",
+"invoiceProject" 	=> "Projects:",
+"invoiceTimePeriod" 	=> "Period of time:",
+"invoiceOptionVat" 	=> " Invoice with VAT",
+"invoiceOptionShort" 	=> "Short Invoice",
+"invoiceOptionRound" 	=> "Round time",
+"invoiceButton" 	=> "Create",
+'noPDOerror'            => 'This extension only works with a PDO database connection.',
+'noZIPerror'            => 'This extension requires either the <a href="http://php.net/manual/en/book.zip.php">PHP zip Extension</a> or needs to execute a zip program on the server.',
+'noData'                => 'There are no time entries for the selected project & time period.'
+
+);
+
+
+?>
