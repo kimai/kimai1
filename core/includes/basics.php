30549ad7b092a457446ea119467b91ee5b509e84
ServiusHack <ServiusHack@6c400f8c-763c-0410-ad1e-de11c01e913c>
2010-07-15 13:37:25 +0000

    Prevent PHP notice about undefined indices during installation.

--- core/includes/basics.php
+++ core/includes/basics.php
@@ -30,18 +30,20 @@ require('func.php');
 require("connect_".$kga['server_conn'].".php");
 
 $vars = var_get_data();
-$kga['currency_name']          = $vars['currency_name'];
-$kga['currency_sign']          = $vars['currency_sign'];
-$kga['show_sensible_data']     = $vars['show_sensible_data'];
-$kga['show_update_warn']       = $vars['show_update_warn'];
-$kga['check_at_startup']       = $vars['check_at_startup'];
-$kga['show_daySeperatorLines'] = $vars['show_daySeperatorLines'];
-$kga['show_gabBreaks']         = $vars['show_gabBreaks'];
-$kga['show_RecordAgain']       = $vars['show_RecordAgain'];
-$kga['show_TrackingNr']        = $vars['show_TrackingNr'];
-$kga['date_format'][0]         = $vars['date_format_0'];
-$kga['date_format'][1]         = $vars['date_format_1'];
-$kga['date_format'][2]         = $vars['date_format_2'];
-if ($vars['language'] != '')
-  $kga['language']             = $vars['language'];
+if (!empty($vars)) {
+  $kga['currency_name']          = $vars['currency_name'];
+  $kga['currency_sign']          = $vars['currency_sign'];
+  $kga['show_sensible_data']     = $vars['show_sensible_data'];
+  $kga['show_update_warn']       = $vars['show_update_warn'];
+  $kga['check_at_startup']       = $vars['check_at_startup'];
+  $kga['show_daySeperatorLines'] = $vars['show_daySeperatorLines'];
+  $kga['show_gabBreaks']         = $vars['show_gabBreaks'];
+  $kga['show_RecordAgain']       = $vars['show_RecordAgain'];
+  $kga['show_TrackingNr']        = $vars['show_TrackingNr'];
+  $kga['date_format'][0]         = $vars['date_format_0'];
+  $kga['date_format'][1]         = $vars['date_format_1'];
+  $kga['date_format'][2]         = $vars['date_format_2'];
+  if ($vars['language'] != '')
+    $kga['language']             = $vars['language'];
+}
 ?>
