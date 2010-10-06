ed7be919c0e178a889d8e982fb2e1e418919100a
ServiusHack <ServiusHack@6c400f8c-763c-0410-ad1e-de11c01e913c>
2010-10-06 11:47:31 +0000

    Added comment regarding Copyright / GPL.

--- core/language/nl.php
+++ core/language/nl.php
@@ -14,9 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Kimai; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * along with Kimai; If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
