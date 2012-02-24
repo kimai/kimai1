93ddf7953d2bfb6fde1c5370f5b4d65cf950bf12
Severin <severin@kimai.org>
2012-02-24 18:33:34 +0100

    Updated Chinese translation file.

--- core/language/zh.php
+++ core/language/zh.php
@@ -1,22 +1,20 @@
 <?php
 /**
- * This file is part of 
+ * This file is part of
  * Kimai - Open Source Time Tracking // http://www.kimai.org
  * (c) 2006-2009 Kimai-Development-Team
- * 
+ *
  * Kimai is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; Version 3, 29 June 2007
- * 
+ *
  * Kimai is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with Kimai; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ * along with Kimai; If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -31,7 +29,6 @@
  */
 
 /*
- *
  * 译者注语：
  *     翻译的很粗糙，主要原因该软件很多功能还未明白应用。多给我一些时间
  *     随着使用的熟练，将会把蹩脚的字面翻译，转换成含有中国文学色彩的解释。
@@ -42,15 +39,17 @@
  * chinese
  * 中文
  *
- * version 0.1 beta
+ * version 1.0 
  *
  * Name: Wu Xiushi
  * Mail: blueswxs@gmail.com
  *
  */
+
+ 
 return array(
 "countryCode"       => "zh-ZH",
-"preferences"       => "参数设置",
+"preferences"       => "设置",
 "total"		        => "汇总",
 "monatzeit"         => "月",
 "heute"             => "今天",
@@ -60,27 +59,30 @@ return array(
 "datum"             => "日期",
 "in"                => "开始",
 "out"               => "结束",
-"time"              => "时‘分",
+"time"              => "时'分",
 "timelabel"         => "时间",
+"starttime"         => "开始时间",
+"DateTimeNotInFuture"=> "日期或时间不能是未来的某个时刻",
 "durationlabel"     => "持续时间",
-"knd"               => "消费者",
+"knd"               => "客户",
 "pct"               => "项目",
-"evt"               => "任务",
-"knds"              => "消费者列表",
+"evt"               => "事件",
+"knds"              => "客户列表",
 "pcts"              => "项目列表",
-"evts"              => "任务列表",
+"evts"              => "事件列表",
 "admin"             => "管理员",
 "rowlimit"          => "最大行数",
 "breiteZeitspalte"  => "时间列字段宽度",
 "breiteUhrspalte"   => "计时列字段宽度",
 "breiteDatumspalte" => "日期列字段宽度",
-"def_pctID"         => "默认项目ID",
-"def_kndID"         => "默认消费者ID",
-"def_evtID"         => "默认事件ID",
+"def_pctID"         => "默认项目编号",
+"def_kndID"         => "默认客户编号",
+"def_evtID"         => "默认事件编号",
 "recommended"       => "建议",
 "submit"            => "完成",
 "cancel"            => "取消",
 "edit"              => "编辑条目",
+"close"             => "关闭",
 "construction"      => "正在构建中...",
 "about"             => "关于",
 "hours"             => "小时",
@@ -88,9 +90,9 @@ return array(
 "secs"              => "秒",
 "skin"              => "皮肤",
 "erase"             => "删除条目",
-"new_knd"           => "添加消费者",
+"new_knd"           => "添加客户",
 "new_pct"           => "添加项目",
-"new_evt"           => "添加任务",
+"new_evt"           => "添加事件",
 "day_summary"       => "时间摘要",
 "plzChoose"         => "请选择",
 "noEntries"         => "在指定的时间范围内没有记录",
@@ -104,18 +106,21 @@ return array(
 "banned"            => "拒绝 (账户已锁定)",
 "tooManyLogins"     => "登录失败次数到达限制，账户锁定.",
 "accessDenied"      => "拒绝访问!",
-"wrongPass"         => "用户名或者秘密错误!",
-"tryAgain"          => "重新登录／找回密码?",
+"wrongPass"         => "帐户名或口令错误!",
+"tryAgain"          => "重新登录／找回口令?",
 "back"              => "返回登录窗口",
 "bye"               => "再见!",
 "login"             => "登录",
 "logout"            => "退出",
-"loggedon"          => "当前登录用户:",
-"username"          => "用户名字",
-"password"          => "密码",
-"newPassword"       => "新密码",
+"loggedon"          => "当前登录账户:",
+"username"          => "账户名",
+"password"          => "口令",
+"newPassword"       => "新口令",
+"retypePassword"    => "确认新口令",
+"passwordsDontMatch"=> "两次口令输入不一致.",
+"passwordTooShort"  => "口令过短小于5个字符.",
 "minLength"         => "最少要求5个字符!",
-"update"            => "安装Kimai时要更新你的数据库，我们备份旧的数据，方便安装失败的时候取回.",
+"update"            => "安装Kimai时要更新你的数据库，我们备份旧的数据，方便安装失败的时候>取回.",
 "updateNow"         => "立刻更新数据!",
 "stop"              => "停止!",
 "start"             => "开始!",
@@ -123,8 +128,12 @@ return array(
 "now"               => "当前",
 "specify"           => "指定列表",
 "add"               => "添加",
-"running"           => "正在运行中!",
+"running"           => "正在运行中...",
 "company"           => "公司",
+
+"vat"               => "税率",
+"contactPerson"     => "联系人",
+
 "street"            => "街道",
 "zipcode"           => "邮编",
 "city"              => "城市",
@@ -133,10 +142,10 @@ return array(
 "mobilephone"       => "移动电话",
 "mail"              => "邮箱",
 "homepage"          => "个人主页",
-"users"             => "用户列表",
+"users"             => "账户列表",
 "name"              => "名字",
 "alias"             => "昵称",
-"adduser"           => "添加用户",
+"adduser"           => "添加账户",
 "addgroup"          => "添加组",
 "group"             => "组",
 "groups"            => "组列表",
@@ -144,7 +153,9 @@ return array(
 "database"          => "数据库",
 "options"           => "选项",
 "status"            => "状态",
-"mode"              => "演示模式 = 0 / 普通模式 (需要登录) = 1",
+"editstatus"		=> "编辑状态",
+"new_status"        => "添加状态",
+"description"       => "描述",
 "adminmail"         => "系统管理员的邮件地址",
 "logintries"        => "登录失败次数",
 "bantime"           => "禁止登录时间单位秒",
@@ -152,14 +163,13 @@ return array(
 "runbackup"         => "现在备份",
 "defaultgrp"        => "默认组(0)",
 "regusr"            => "普通用户",
-"leaderusr"         => "特权用户",
-"adminusr"          => "管理用户",
+"adminusr"          => "特权用户",
 "editusr"           => "编辑用户",
 "editgrp"           => "编辑组",
 "delgrp"            => "删除组",
 "delpct"            => "删除项目",
-"delknd"            => "删除消费者",
-"delevt"            => "删除任务",
+"delknd"            => "删除客户",
+"delevt"            => "删除事件",
 "switchusr"         => "切换这个账户",
 "backupusr"         => "备份这个账户",
 "mailusr"           => "发送邮件到这个账户",
@@ -169,12 +179,15 @@ return array(
 "changegrp"         => "更改组",
 "members"           => "成员列表",
 "groupleader"       => "组管理员",
-"nopasswordset"     => "空密码设置",
+"nopasswordset"     => "空口令设置!",
+"nopassword"        => "空口令",
 "noItems"           => "空条目",
 "DBname"            => "数据库名字",
-"updated"           => "数据记录更新!",
 "autoselection"     => "自动选择",
-"quickdelete"       => "快速删除",
+"quickdelete"       => "快速删除表中的条目。",
+"quickdeleteHide"   => "否",
+"quickdeleteShow"   => "是的, 但不显示提示。",
+"quickdeleteShowConfirm" => "是的，但显示提示。",
 "tools"             => "工具",
 "print"             => "打印",
 "save"              => "保存",
@@ -193,52 +206,72 @@ return array(
 
 "ac"                => "自动重复输入.",
 "ac_error"          => "自动重复输入错误 (时间范围太长).",
+"refundable_long"   => "返还 (针对员工)",
+"refundable"        => "返还",
+"yes"               => "是",
+"no"                => "否",
 "comment_type"      => "备注类型",
 "ctype0"            => "注解",
 "ctype1"            => "通知",
 "ctype2"            => "警告",
 "hint"              => "命中:",
-"usr_caution1"      => "If you change the user-name of",
-"usr_caution2"      => "you have to re-login under your new username immediately!",
-"timediff_warn"     => "You must enter different times into the time in-/out fields!",
+"usr_caution1"      => "如果要更改的账户名是",
+"usr_caution2"      => "新账户重新登录才能生效!",
+"timediff_warn"     => "注意开始结束时间字段不能相同!",
 "visibility"        => "是否显示",
 "showitem"          => "显示",
-"hideitem"          => "不显示",
-"checkUsername"     => "必须指定用户名!",
+"hideitem"          => "隐藏",
+"checkUsername"     => "必须指定账户名!",
+"checkGroupname"    => "必须指定组名!",
+"checkStatusname"    => "必须指定状态名!",
 "clickToReload"     => "点击重载窗口",
 "sure"              => "确定?",
 "clock"             => "时钟",
 "currSum"           => "时间范围描述摘要",
 "admWarn"           => "你是普通管理员，只能设置其他低级别管理员!",
-"flip_pct_display"  => "Order project table by customers",
-"pct_comment_flag"  => "Always show comments beside projects",
-"showIDs"           => "显示全部ID",
+"flip_pct_display"  => "所有客户的项目列表",
+"pct_comment_flag"  => "总是在项目旁边显示评论信息",
+"showIDs"           => "显示全部编号",
 "error"             => "错误记录!",
 "checkupdate"       => "锁住更新",
 
 "selectR"           => "选择要操作的记录条目:",
-"selectKND"         => "消费者:",
-"selectPCT"         => "项目:",
-"selectEVT"         => "任务:",
+"selectKND"         => "C:",
+"selectPCT"         => "P:",
+"selectEVT"         => "T:",
 
-"testKND"           => "测试－消费者",
-"testPCT"           => "测试－项目",
-"testEVT"           => "测试",
+"testKND"           => "测试 - 客户",
+"testPCT"           => "测试 - 项目",
+"testEVT"           => "测试事件",
 
+"time_billable"     => "计费",
+"billable"          => "计费",
 "budget"            => "预制",
 "rate"              => "速率 (每小时)",
 "rate_short"        => "速率",
+"approved"          => "标准",
 "default_rate"      => "默认速率",
 "my_rate"           => "我的速率",
+"fixed_rate"        => "修正速率",
+"used"          	=> "使用",
+"budget_available" 	=> "可用预算",
+"budget_event" 		=> "事件预算",
+"budget_event_used" => "已用时间预算",
+"budget_event_approved" => "预算标准",
+"budget_minus"		=> "预算超支",
+"wage"              => "工资",
+"roundTimesheetEntries" => "新计时条目图",
+"minutes"			=> "分钟",
+"seconds" 			=> "秒",
+"and" 				=> "和",
+"customerlogin"     => "客户登录",
 
-"wage"              => "进行",
-
-"customerlogin"     => "消费者登录",
-
-"expense"           => "消费",
+"expense"           => "计费",
 "designation"       => "命名",
 
 "cleared"           => "清除",
+
+"atLeastOneGroup" => '至少要选择一个组.',
 
 "noFading"          => "非淡出效果",
 "user_list_hidden"  => "默认隐藏用户列表",
@@ -259,56 +292,145 @@ return array(
 'round_time' => '周期时间到',
 'round_time_minute' => ' 分.',
 'logged_in_as' => '登录在:',
-
 'decimal_separator'  => '十进制分隔符',
 
 'view_filter' => '显示过滤',
 'unassigned' => '未指定',
 'all_events' => '&lt;&lt;all&gt;&gt;',
-);
+'all' => '全部',
 
-$kga['lang']['months']         =
-array("一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月");
-$kga['lang']['months_short']   =
-array("一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月");
-$kga['lang']['weekdays']       = array("星期日","星期一","星期二","星期三","星期四","星期五","星期六");
-$kga['lang']['weekdays_short'] = array("周日","周一","周二","周三","周四","周五","周六");
+'durationWithSeconds' => '持续时间精确到秒.',
 
-$kga['lang']['errors'][0]['hdl'] = "数据不能联接!";
-$kga['lang']['errors'][0]['txt'] = "先确认数据库已运行，然后PDO配置激活，配置文件'conf.php'.";
+'multiplier' => '相乘',
 
-$kga['lang']['errors'][1]['hdl'] = "安装时有错误!";
-$kga['lang']['errors'][1]['txt'] = 'Kimai 数据表已经存在无法创建，请先清除后再次安装.';
+'dateAndTimeHelp' => '<strong>时间和日期参考下面的格式:</strong><br />
+            Dates: DD.MM.YYYY<br />
+            Times: HH:MM:SS or HH:MM or just HH. Ommitted parts will be set to 0.',
 
-$kga['lang']['updater'][0]        = "是的, 我有一个kimai要备份! 开始更新中!";
-$kga['lang']['updater'][10]       = "图例:";
-$kga['lang']['updater'][20]       = "查询成功.";
-$kga['lang']['updater'][30]       = "查询失败- 大多数是不相干的问题.";
-$kga['lang']['updater'][40]       = "查询失败.";
-$kga['lang']['updater'][50]       = "备份日志:";
-$kga['lang']['updater'][60]       = "备份失败，更新放弃!";
-$kga['lang']['updater'][70]       = "更新日志:";
-$kga['lang']['updater'][80]       = "数据更新成功并且一个备份已创建.";
-$kga['lang']['updater'][90]       = "问题处理.";
-$kga['lang']['updater'][100]      = "数据更新出现致命错误，数据库重新建立前kimai不能工作.";
-$kga['lang']['updater'][110]      = "新的密码";
-$kga['lang']['updater'][120]      = "所有用户都使用这个新的密码.";
-$kga['lang']['updater'][130]      = "不能写入autoconf.php.";
-$kga['lang']['updater'][140]      = "更新文件autoconf.php";
+'timezone' => '时区',
+'defaultTimezone' => '默认新账户和客户时区',
+'editLimitPart1' => '没有条目可以被添加或修改.',
+'editLimitPart2' => '天 和',
+'editLimitPart3' => '已用小时数.',
+'editLimitError' => '结束日期要在将来的某一时刻.',
 
-$kga['lang']['backup'][0]        = "这个数据备份没有经过测试，存在风险的.";
-$kga['lang']['backup'][1]        = "可用的备份:";
-$kga['lang']['backup'][2]        = "回滚";
-$kga['lang']['backup'][3]        = "删除";
-$kga['lang']['backup'][4]        = "完成";
-$kga['lang']['backup'][5]        = "你只能回滚到一个时间的数据!";
-$kga['lang']['backup'][6]        = "备份来自";
-$kga['lang']['backup'][7]        = "回滚数据.";
-$kga['lang']['backup'][8]        = "创建备份";
-$kga['lang']['backup'][9]        = "数据备份只是一个数据快照，不同于底层数据库备份，根据需要选定备份策略.";
+'hideClearedEntries' => '隐藏清除条目',
+'showCommentsByDefault' => '默认显示评论',
+'hideOverlapLines' => '隐藏时间重叠的条目',
+
+'general' => '基本',
+'address' => '地址',
+'contact' => '联系方式',
+'list'=> '列表',
+'sublists' => '子列表',
+
+'internalProject' => '内部项目',
+
+'exactSums' => '不应该使用四舍五入后的数值，应该使用精确数值.',
+
+'securityWarning' => '安全警告',
+'installerWarningHeadline' => '安装程序文件夹仍然存在!',
+'installerWarningText' => '安装完成后出于安全应该删除installer目录。',
+
+'change' => '更改',
+
+'sublistAnnotations' => '子列表注释',
+
+'TimeDateInputError' => '时间或日期不是有效的.',
+
+'StartTimeBeforeEndTime' => '开始时间一定要在结束时间之前.',
+
+'taskAssignable' => '可转让',
+'taskAssignableDescription' => '项目中可转让',
 
 
-$kga['lang']['xp_ext'] = array(
+'credits' => <<<EOF
+        Torsten H&ouml;ltge, Severin Leonhardt, Kevin Papst, Oleg Britvin, Martin Klemkow and others...<br />
+        
+        <br />
+        
+        <strong>This program is free software and published 'AS IS' without any kind of warranty</strong> under the terms of the
+        <a href="../COPYING">GPL Version 3</a>. 
+        
+        <br /><br />
+         
+        <a href='http://www.kimai.org' target='_blank'>Official Website</a> | 
+        <a href='http://forum.kimai.org/' target='_blank'>Forum</a> |
+        <a href='http://sourceforge.net/projects/kimai/' target='_blank'>Sourceforge Project page</a> | 
+        <a href='http://sourceforge.net/projects/kimai/develop' target='_blank'>Subversion Repository</a> |
+        <a href='http://www.kimai.org/en/download.html' target='_blank'>Download</a>
+        
+        <br /><br />
+
+        If you do any alternations we would appreciate a posting on the <a href="http://forum.kimai.org" title="Kimai Forum">forum</a>. You'll find a special board "<a href="http://forum.kimai.org/index.php?board=10.0" title="Extensions / Friendly Hacks">Friendly Hacks</a>" there. Latest SVN Version can be downloaded <a href="http://kimai.svn.sourceforge.net/viewvc/kimai.tar.gz?view=tar">here</a> (download for testing purposes only!).
+        
+        <br /><br />
+        
+        Please support further development of Kimai with a <a href="http://www.kimai.org/en/donate.html">donation</a> or by extending its <a href="http://kimai.svn.sourceforge.net/viewvc/kimai/kimai/core/language/">language library</a>.
+
+        If you find bugs or have ideas for features, feel free to contact us through the forum. Or even better: create a <a href="http://sourceforge.net/tracker/?group_id=186214">ticket</a>!<br /><br />
+
+        <strong>Further thanks for support to:</strong> Vasilis van Gemert, Maximilian Kern, Enrico Ties, Thomas Wensing, John Resig, Kelvin Luck, Urs Gerig, Willem van Gemert,
+        Torben Boe and HamBug Studios, Klaus Franken, Chris (Urban Willi), Andreas Berndt, Niels Hoffmann, G&uuml;nter Hengsbach, Paul Brand, Joaqu&iacute;n G. de la Zerda,
+        Allesandro Bertoldo, Jos&eacute; Ricardo Cardoso.<br /><br />
+
+        <strong>Special thanks</strong> to all beta testers, which are way to many to mention them all - you know who you are! ;)<br /><br />
+
+        The invoice extension uses the PclZip Library, which can be found at <a href="http://www.phpconcept.net" rel="nofollow">http://www.phpconcept.net</a>.<br/><br/>
+
+        Version 0.8 was emerged with the kind support of RRZE (Regionales Rechenzentrum Erlangen).<br /><br />
+        <br />
+EOF
+,
+
+
+'months' => array("一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"),
+'months_short' => array("一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"),
+'weekdays' => array("星期日","星期一","星期二","星期三","星期四","星期>五","星期六"),
+'weekdays_short' => array("周日","周一","周二","周三","周四","周五","周六"),
+
+
+'errors' => array (
+  0 => array ('hdl' => "数据库不能联接!",
+              'txt' => "先确认数据库已运行，然后PDO配置激活，配置文件'autoconf.php'."),
+
+  1 => array ('hdl' => "安装时有错误!",
+              'txt' => 'Kimai 数据表已经存在无法创建，请先清除后再次安装.')
+  ),
+
+'updater' => array (
+0        => "是的, 我有一个kimai要备份! 开始更新中!",
+10       => "图例:",
+20       => "查询成功.",
+30       => "查询失败- 大多数是不相干的问题.",
+40       => "查询失败.",
+50       => "备份日志:",
+60       => "备份失败，更新放弃!",
+70       => "更新日志:",
+80       => "数据更新成功并且一个备份已创建.",
+90       => "问题处理.",
+100      => "数据更新出现致命错误，数据库重新建立前kimai不能工作.",
+110      => "新的密码",
+120      => "所有用户都使用这个新的密码.",
+130      => "不能写入autoconf.php.",
+140      => "更新文件autoconf.php",
+'timezone' => 'mai 现在支持时区,请选择您的默认时区,它将用于所有用户和客户,用户可以更改时区的时间.',
+),
+
+'backup' => array(
+0        => "这个数据备份没有经过测试，存在风险的.",
+1        => "可用的备份:",
+2        => "回滚",
+3        => "删除",
+4        => "完成",
+5        => "你只能回滚到一个时间的数据!",
+6        => "备份来自",
+7        => "回滚数据",
+8        => "创建备份",
+9        => "数据备份只是一个数据快照，不同于底层数据库备份，根据需要选定备份策略."),
+
+
+'xp_ext' => array(
 	
 "print_hint"      		   => "点击打开一个打印表格.",
 "dl_hint"         		   => "点击下载该输出文件.",
@@ -318,19 +440,25 @@ $kga['lang']['xp_ext'] = array(
 "tab_timeformat"	 	   => "时间格式",
                               
 "cleared_all"	 		   => "所有",
-"cleared_cleared" 		   => "只清楚",
+"cleared_cleared" 		   => "只清除",
 "cleared_open"	 		   => "只打开",
-"stdrd_location"  		   => "标准位置",
+"stdrd_location"  		   => "标准定位",
 "timeformat"	     	   => "时间格式",
 "dateformat"			   => "日期格式",
+
+"filter"	 		       => "过滤",
+
+"refundable_all"	 	   => "全部",
+"refundable_refundable"    => "可返还",
+"refundable_not_refundable"=> "不可返回",
                               
 "print_comment"   		   => "打印备注",
 "print_summary"   		   => "打印摘要",
 "create_bookmarks"		   => "创建书签",
 "download_pdf"    		   => "下载这个PDF",
-"pdf_format"             => "格式",
-"export_pdf"             => "表",
-"export_pdf2"            => "显示",
+"pdf_format"               => "格式",
+"export_pdf"               => "表",
+"export_pdf2"              => "显示",
                               
 "column_delimiter"		   => "分隔字符数量",
 "quote_char"      		   => "分隔字符段",
@@ -343,43 +471,59 @@ $kga['lang']['xp_ext'] = array(
 
 'subtotal'              => '子类汇总',
 'finalamount'           => '汇总',
-'pdf_headline'          => '显示正在占用的时间与消费',
+'pdf_headline'          => '显示正在占用的计时与计费',
+'pdf_headline_only_times'   => '计时列表',
+'pdf_headline_only_expenses'=> '计费列表',
 'costs'                 => '费用',
-'expense'               => '消费',
+'expense'               => '计费',
 'summary'               => '摘要',
+'full_list'             => '全部列表',
 'page'                  => '页',
 'by'                    => '在',
 'done_by'               => '完成在',
 'duration'              => '期间',
-'hide_cleared_entries'  => '不显示带清除标记的记录',
 'customer_new_page'     => '新建消费者页面',
 
-'times_and_expenses' => '时间与消费时间列表',
-'times' => '时间列表',
-'expenses' => '消费时间列表',
+'times_and_expenses' => '计时与计费列表',
+'times' => '计时列表',
+'expenses' => '计费列表',
 
 'reverse_order' => '旧的数据记录条目优先',
 'time_period'  => '时间周期范围',
 
 'duration_unit' => '小时',
 
-);
+),
 
 
 
-$kga['lang']['ext_invoice'] = array(
+'ext_invoice' => array(
 	
-"invoiceTitle" 			=> "创建明细报表",
+"invoiceTemplate"           => "账单模板:",
+"invoiceTitle" 			=> "创建账单明细",
 "invoiceProject" 		=> "项目列表:",
 "invoiceTimePeriod" 	=> "时间周期范围:",
-"invoiceOptionVat" 		=> "完整明细表",
 "invoiceOptionShort" 	=> "简化明细表",
 "invoiceOptionRound" 	=> "柱形明细表",
 "invoiceButton" 		=> "创建",
-'noPDOerror'            => '错误: 只能通过PDO(php ... )连接数据库才能工作.',
-'noZIPerror'            => '错误: 需要有<a href="http://php.net/manual/en/book.zip.php">PHP zip Extension</a>
-支持或者需要在服务器上执行zip程序才能解决.',
-'noData'                => '选择的项目在指定的时间周期内没有数据记录条目.'
+'noData'                => '选择的项目在指定的时间周期内没有数据记录条目..',
+'defaultVat'        => '默认税率'
+),
+
+'ext_budget' => array(
+  'unusedBudget' => '未使用'
+),
+
+'extensions' => array(
+  'ki_timesheet' => '计时统计列表',
+  'ap_ext' => '管理控制面板',
+  'ki_expenses' => '计费统计列表',
+  'ki_export' => '列表格式导出',
+  'ki_budget' => '饼状图统计显示',
+  'deb_ext' => '调试数据',
+  'demo_ext' => '扩展功能显示',
+  'ki_invoice' => '账单格式导出' 
+)
 
 );
 
