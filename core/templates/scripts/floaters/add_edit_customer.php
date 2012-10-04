b92143d511c7af817d1a22cf48dc3009957c669f
kevinpapst <kpapst@gmx.net>
2012-10-04 23:19:38 +0200

    fixed several html issues

--- core/templates/scripts/floaters/add_edit_customer.php
+++ core/templates/scripts/floaters/add_edit_customer.php
@@ -57,7 +57,7 @@
                       <span class="cc">&nbsp;</span>
                       </a></li>
 <?php endif; ?>
-          <li class="tab norm"><a href="#comment">
+          <li class="tab norm"><a href="#commenttab">
                       <span class="aa">&nbsp;</span>
                       <span class="bb"><?php echo $this->kga['lang']['comment']?></span>
                       <span class="cc">&nbsp;</span>
@@ -76,146 +76,116 @@
     <div id="floater_tabs" class="floater_content">
 
             <fieldset id="general">
-
                 <ul>
-                
                     <li>
                         <label for="name" ><?php echo $this->kga['lang']['customer']?>:</label>
-            <?php echo $this->formText('name', $this->name);?>
+                        <?php echo $this->formText('name', $this->name);?>
                     </li>
-
                     <li>
                         <label for="vat" ><?php echo $this->kga['lang']['vat']?>:</label>
-            <?php echo $this->formText('vat', $this->vat);?>
-                    </li> 
-
+                        <?php echo $this->formText('vat', $this->vat);?>
+                    </li>
                     <li>
                          <label for="visible"><?php echo $this->kga['lang']['visibility']?>:</label>
-            <?php echo $this->formCheckbox('visible', '1',array('checked' => $this->visible || !$this->id));?>
-                    </li>   
-
+                         <?php echo $this->formCheckbox('visible', '1',array('checked' => $this->visible || !$this->id));?>
+                    </li>
                     <li>
                       <label for="password"><?php echo $this->kga['lang']['password']?>:</label>
                       <div class="multiFields">
-            <?php echo $this->formPassword('password', '', array(
-		'cols' => 30,
-		'rows' => 3,
-		'disabled' => !$this->password?'disabled':''
-));?><br/>
-            <?php echo $this->formCheckbox('no_password', '1',array('class' => 'disableInput', 'checked' => !$this->password)); echo $this->kga['lang']['nopassword']?>
+                        <?php echo $this->formPassword('password', '', array(
+                                    'cols' => 30,
+                                    'rows' => 3,
+                                    'disabled' => !$this->password?'disabled':''
+                            ));?><br/>
+                        <?php echo $this->formCheckbox('no_password', '1',array('class' => 'disableInput', 'checked' => !$this->password)); echo $this->kga['lang']['nopassword']?>
                       </div>
-                    </li> 
-
-                    <li>
-                      <label for="timezone"><?php echo $this->kga['lang']['timezone']?>:</label>
-              <?php echo $this->formSelect('timezone', $this->timezone, null, $this->timezones); ?>
                     </li>
-
+                    <li>
+                        <label for="timezone"><?php echo $this->kga['lang']['timezone']?>:</label>
+                        <?php echo $this->formSelect('timezone', $this->timezone, null, $this->timezones); ?>
+                    </li>
                 </ul>
-                
             </fieldset>
 
-            <fieldset id="comment">
-
+            <fieldset id="commenttab">
                 <ul>
-
                     <li>
-                         <label for="comment"><?php echo $this->kga['lang']['comment']?>:</label>
-            <?php echo $this->formTextarea('comment', $this->comment,array(
-		'cols' => 30,
-		'rows' => 5,
-		'class' => 'comment'
-		));?>
-                    </li>   
-
+                        <label for="comment"><?php echo $this->kga['lang']['comment']?>:</label>
+                        <?php echo $this->formTextarea('comment', $this->comment,array(
+                            'cols' => 30,
+                            'rows' => 5,
+                            'class' => 'comment'
+                            ));?>
+                    </li>
                 </ul>
-                
             </fieldset>
 
 <?php if (count($this->groupIDs) > 1): ?>
             <fieldset id="groups">
-
                 <ul>
-                    
                     <li>
                         <label for="customerGroups" ><?php echo $this->kga['lang']['groups']?>:</label>
-              <?php echo $this->formSelect('customerGroups[]', $this->selectedGroups, array(
-		'class' => 'formfield',
-		'id' => 'activityGroups',
-		'multiple' => 'multiple',
-		'size' => 3,
-		'style' => 'width:255px'), $this->groups); ?>
+                        <?php echo $this->formSelect('customerGroups[]', $this->selectedGroups, array(
+                            'class' => 'formfield',
+                            'id' => 'activityGroups',
+                            'multiple' => 'multiple',
+                            'size' => 3,
+                            'style' => 'width:255px'), $this->groups); ?>
                     </li>
-
                 </ul>
-                
             </fieldset>
 <?php else: 
  echo $this->formHidden('customerGroups[]', $this->selectedGroups[0], null ,array('id' => 'customerGroups'));
 endif; ?>
 
             <fieldset id="address">
-
                 <ul>
-
                     <li>
                         <label for="company" ><?php echo $this->kga['lang']['company']?>:</label>
-            <?php echo $this->formText('company', $this->company);?>
+                        <?php echo $this->formText('company', $this->company);?>
                     </li>
-
                     <li>
                         <label for="contactPerson" ><?php echo $this->kga['lang']['contactPerson']?>:</label>
-            <?php echo $this->formText('contactPerson', $this->contact);?>
+                        <?php echo $this->formText('contactPerson', $this->contact);?>
                     </li>
-                                      
                     <li>
                         <label for="street" ><?php echo $this->kga['lang']['street']?>:</label>
-            <?php echo $this->formText('street', $this->street);?>
+                        <?php echo $this->formText('street', $this->street);?>
                     </li>
-                          
                     <li>
                         <label for="zipcode" ><?php echo $this->kga['lang']['zipcode']?>:</label>
-            <?php echo $this->formText('zipcode', $this->zipcode);?>
+                        <?php echo $this->formText('zipcode', $this->zipcode);?>
                     </li>
-                          
                     <li>
                         <label for="city" ><?php echo $this->kga['lang']['city']?>:</label>
-            <?php echo $this->formText('city', $this->city);?>
-                    </li>     
-
+                        <?php echo $this->formText('city', $this->city);?>
+                    </li>
                 </ul>
-                
             </fieldset>
 
             <fieldset id="contact">
-
-                <ul>   
-                          
+                <ul>
                     <li>
                         <label for="phone" ><?php echo $this->kga['lang']['telephon']?>:</label>
-            <?php echo $this->formText('phone', $this->phone);?>
+                        <?php echo $this->formText('phone', $this->phone);?>
                     </li>        
                           
                     <li>
                         <label for="fax" ><?php echo $this->kga['lang']['fax']?>:</label>
-            <?php echo $this->formText('fax', $this->fax);?>
-                    </li>        
-                          
+                        <?php echo $this->formText('fax', $this->fax);?>
+                    </li>
                     <li>
                         <label for="mobile" ><?php echo $this->kga['lang']['mobilephone']?>:</label>
-            <?php echo $this->formText('mobile', $this->mobile);?>
-                    </li>        
-                          
+                        <?php echo $this->formText('mobile', $this->mobile);?>
+                    </li>
                     <li>
                         <label for="mail" ><?php echo $this->kga['lang']['mail']?>:</label>
-            <?php echo $this->formText('mail', $this->mail);?>
-                    </li>        
-                          
+                        <?php echo $this->formText('mail', $this->mail);?>
+                    </li>
                     <li>
                         <label for="homepage" ><?php echo $this->kga['lang']['homepage']?>:</label>
-            <?php echo $this->formText('homepage', $this->homepage);?>
-                    </li>        
-
+                        <?php echo $this->formText('homepage', $this->homepage);?>
+                    </li>
                 </ul>
                 
             </fieldset>
