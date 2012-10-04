00225360505a64785cd72ee38437fca45ee8b3e3
kevinpapst <kpapst@gmx.net>
2012-10-04 22:59:38 +0200

    fixed several html issues

--- core/templates/scripts/floaters/add_edit_activity.php
+++ core/templates/scripts/floaters/add_edit_activity.php
@@ -44,7 +44,7 @@ $(document).ready(function() {
                       <span class="bb"><?php echo $this->kga['lang']['general']?></span>
                       <span class="cc">&nbsp;</span>
                       </a></li>
-          <li class="tab norm"><a href="#projects">
+          <li class="tab norm"><a href="#projectstab">
                       <span class="aa">&nbsp;</span>
                       <span class="bb"><?php echo $this->kga['lang']['projects']?></span>
                       <span class="cc">&nbsp;</span>
@@ -56,121 +56,108 @@ $(document).ready(function() {
                       <span class="cc">&nbsp;</span>
                       </a></li>
 <?php endif; ?>
-          <li class="tab norm"><a href="#comment">
+          <li class="tab norm"><a href="#commenttab">
                       <span class="aa">&nbsp;</span>
                       <span class="bb"><?php echo $this->kga['lang']['comment']?></span>
                       <span class="cc">&nbsp;</span>
                       </a></li>
       </ul>
     </div>
-    
-        <form id="add_edit_activity" action="processor.php" method="post"> 
-                
-                <input name="activityFilter"   type="hidden" value="0" />
- 
-                <input name="axAction" type="hidden" value="add_edit_CustomerProjectActivity" />   
-                <input name="axValue" type="hidden" value="activity" />   
-                <input name="id" type="hidden" value="<?php echo $this->id; ?>" />   
+
+    <form id="add_edit_activity" action="processor.php" method="post">
+
+        <input name="activityFilter"   type="hidden" value="0" />
+
+        <input name="axAction" type="hidden" value="add_edit_CustomerProjectActivity" />
+        <input name="axValue" type="hidden" value="activity" />
+        <input name="id" type="hidden" value="<?php echo $this->id; ?>" />
 
 
-    <div id="floater_tabs" class="floater_content">
-
-
+        <div id="floater_tabs" class="floater_content">
             <fieldset id="general">
-
                 <ul>
-                
                     <li>
                         <label for="name" ><?php echo $this->kga['lang']['activity']?>:</label>
-            <?php echo $this->formText('name', $this->name);?>
+                        <?php echo $this->formText('name', $this->name);?>
                     </li>
-                
                     <li>
                         <label for="defaultRate" ><?php echo $this->kga['lang']['default_rate']?>:</label>
-            <?php echo $this->formText('defaultRate', str_replace('.',$this->kga['conf']['decimalSeparator'],$this->defaultRate)); ?>
+                        <?php echo $this->formText('defaultRate', str_replace('.',$this->kga['conf']['decimalSeparator'],$this->defaultRate)); ?>
                     </li>
-                
                     <li>
                         <label for="myRate" ><?php echo $this->kga['lang']['my_rate']?>:</label>
-            <?php echo $this->formText('myRate', str_replace('.',$this->kga['conf']['decimalSeparator'],$this->myRate)); ?>
+                        <?php echo $this->formText('myRate', str_replace('.',$this->kga['conf']['decimalSeparator'],$this->myRate)); ?>
                     </li>
-                
                     <li>
-                        <label for="myRate" ><?php echo $this->kga['lang']['fixedRate']?>:</label>
-            <?php echo $this->formText('fixedRate', str_replace('.',$this->kga['conf']['decimalSeparator'],$this->fixedRate)); ?>
+                        <label for="fixedRate" ><?php echo $this->kga['lang']['fixedRate']?>:</label>
+                        <?php echo $this->formText('fixedRate', str_replace('.',$this->kga['conf']['decimalSeparator'],$this->fixedRate)); ?>
                     </li>
-
                     <li>
                          <label for="visible"><?php echo $this->kga['lang']['visibility']?>:</label>
-            <?php echo $this->formCheckbox('visible', '1',array('checked' => $this->visible || !$this->id));?>
+                        <?php echo $this->formCheckbox('visible', '1',array('checked' => $this->visible || !$this->id));?>
                     </li>
                  </ul>
-
             </fieldset>
-            
-            <fieldset id="comment">
+
+            <fieldset id="commenttab">
                 <ul>
                     <li>
                          <label for="comment"><?php echo $this->kga['lang']['comment']?>:</label>
-            <?php echo $this->formTextarea('comment', $this->comment,array(
-		'cols' => 30,
-		'rows' => 5,
-		'class' => 'comment'
-		));?>
+                         <?php echo $this->formTextarea('comment', $this->comment,array(
+                            'cols' => 30,
+                            'rows' => 5,
+                            'class' => 'comment'
+                            ));?>
                     </li>
                 </ul>
             </fieldset>
-          
 
-<?php if (count($this->groupIDs) > 1): ?>   
-    <fieldset id="groups">
+
+    <?php if (count($this->groupIDs) > 1) { ?>
+            <fieldset id="groups">
                 <ul>
-                 
                     <li>
-                        <label for="activityGroups" ><?php echo $this->kga['lang']['groups']?>:</label>
-              <?php echo $this->formSelect('activityGroups[]', $this->selectedGroups, array(
-		'class' => 'formfield',
-		'id' => 'activityGroups',
-		'multiple' => 'multiple',
-		'size' => 3,
-		'style' => 'width:255px'), $this->groups); ?>
-                    </li>      
+                        <label for="activityGroups"><?php echo $this->kga['lang']['groups']?>:</label>
+                        <?php echo $this->formSelect('activityGroups[]', $this->selectedGroups, array(
+                            'class' => 'formfield',
+                            'id' => 'activityGroups',
+                            'multiple' => 'multiple',
+                            'size' => 3,
+                            'style' => 'width:255px'), $this->groups); ?>
+                    </li>
                 </ul>
             </fieldset>
-<?php else:
- echo $this->formHidden('activityGroups[]', $this->selectedGroups[0], array('id' => 'activityGroups'));
-endif; ?>
+    <?php
+        } else {
+            echo $this->formHidden('activityGroups[]', $this->selectedGroups[0], array('id' => 'activityGroups'));
+        }
+    ?>
 
-    <fieldset id="projects">
+            <fieldset id="projectstab">
                 <ul>
-
                     <li>
-                        <label for="projects"><?php echo $this->kga['lang']['projects']?>:</label>
-              <?php echo $this->formSelect('projects[]', $this->selectedProjects, array(
-		'class' => 'formfield',
-		'id' => 'activityProjects',
-		'multiple' => 'multiple',
-		'size' => 15,
-		'disabled' => !$this->assignable?'disabled':'',
-		'style' => 'width:255px'), $this->projects); ?>
+                        <label for="activityProjects"><?php echo $this->kga['lang']['projects']?>:</label>
+                        <?php echo $this->formSelect('projects[]', $this->selectedProjects, array(
+                            'class' => 'formfield',
+                            'id' => 'activityProjects',
+                            'multiple' => 'multiple',
+                            'size' => 5,
+                            'disabled' => !$this->assignable?'disabled':'',
+                            'style' => 'width:255px'), $this->projects); ?>
                     </li>
-
                     <li>
-                        <label for="assignable" ><?php echo $this->kga['lang']['taskAssignable']?>:</label>
-            <?php echo $this->formCheckbox('assignable', '1',array('checked' => $this->assignable,'class' => 'disableInput formfield'));
-                echo $this->kga['lang']['taskAssignableDescription']?>
+                        <label for="assignable"><?php echo $this->kga['lang']['taskAssignable']?>:</label>
+                        <?php echo $this->formCheckbox('assignable', '1',array('checked' => $this->assignable,'class' => 'disableInput formfield'));
+                            echo $this->kga['lang']['taskAssignableDescription']?>
                     </li>
-
-
-                    
                 </ul>
-      </fieldset>
-        
-    </div>
-                                             
-                <div id="formbuttons">
-                    <input class='btn_norm' type='button' value='<?php echo $this->kga['lang']['cancel']?>' onClick='floaterClose(); return false;' />
-                    <input class='btn_ok' type='submit' value='<?php echo $this->kga['lang']['submit']?>'/>
-                </div>
-        </form>
+            </fieldset>
+
+        </div>
+
+        <div id="formbuttons">
+            <input class='btn_norm' type='button' value='<?php echo $this->kga['lang']['cancel']?>' onClick='floaterClose(); return false;' />
+            <input class='btn_ok' type='submit' value='<?php echo $this->kga['lang']['submit']?>'/>
+        </div>
+    </form>
 </div>
