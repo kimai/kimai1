a6cb8faba16f470d842e9c60f9ee4b935476a331
kevinpapst <kpapst@gmx.net>
2012-10-05 19:56:04 +0200

    fixed label ids

--- core/templates/scripts/login/panel.php
+++ core/templates/scripts/login/panel.php
@@ -1,14 +1,14 @@
         <div id='login'>
             <form action='index.php?a=checklogin' name='form1' method='post'>
                 <fieldset>
-                    <label for='kimaiusername'>
+                    <label for="kimaiusername">
                         <?php echo $this->kga['lang']['username']?>:
                     </label>
-                    <input type='text' name='name' />
-                    <label for='kimaipassword'>
+                    <input type='text' name="name" id="kimaiusername" />
+                    <label for="kimaipassword">
                         <?php echo $this->kga['lang']['password']?>:
                     </label>
-                    <input type='password' name='password' />
+                    <input type='password' name="password" id="kimaipassword" />
                     <?php echo $this->selectbox ?>
                     <button type='submit'>Submit</button>
                 </fieldset>
@@ -16,8 +16,8 @@
 
             
             <div id="warning">
-                <p id="JSwarning"><strong style='color:red'><?php $this->kga['lang']['JSwarning']?></strong></p>
-                <p id="cookiewarning"><strong style='color:red'><?php $this->kga['lang']['cookiewarning']?></strong></p>
+                <p id="JSwarning"><strong style="color:red"><?php $this->kga['lang']['JSwarning']?></strong></p>
+                <p id="cookiewarning"><strong style="color:red"><?php $this->kga['lang']['cookiewarning']?></strong></p>
             </div>
             <?php echo $this->partial('misc/copyrightnotes.php', array('kga' => $this->kga, 'devtimespan' => $this->devtimespan)); ?>
             <!-- 
