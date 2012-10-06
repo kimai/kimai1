4811db92d7490cc23aa92e10e65072596f05bc58
kevinpapst <kpapst@gmx.net>
2012-10-06 19:31:43 +0200

    refactored code to use view helper and fixed undefined index if no entries were available

--- core/templates/scripts/lists/customers.php
+++ core/templates/scripts/lists/customers.php
@@ -1,45 +1,53 @@
 <table>
   <tbody>
-
-    <?php foreach ($this->customers as  $customer):
-    if (!$customer['visible']) continue; ?>
-    
-    <tr id="row_customer<?php echo $customer['customerID']?>" class="customer customer<?php echo $customer['customerID']?> <?php echo $this->cycle(array('odd','even'))->next()?>">
-
-      <!-- option cell -->
-      <td nowrap class="option">            
-        <?php if ($this->kga['user'] && $this->kga['user']['status'] != 2): ?>
-        <a href ="#" onClick="editSubject('customer',<?php echo $customer['customerID']?>); $(this).blur(); return false;">
-          <img src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/edit2.gif' width='13' height='13' alt='<?php echo $this->kga['lang']['edit']?>' title='<?php echo $this->kga['lang']['edit']?>' border='0' />
-        </a>
-        <?php else: ?>
-        <a href ="#" onClick="lists_update_filter('customer',<?php echo $customer['customerID']?>); $(this).blur(); return false;">
-          <img src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/filter.png' width='13' height='13' alt='<?php echo $this->kga['lang']['filter']?>' title='<?php echo $this->kga['lang']['filter']?>' border='0' />
-        </a>
-        <?php endif; ?>
-      </td>
-
-      <!-- name cell -->
-      <td width="100%" class="clients" onmouseover="lists_change_color(this,true);" onmouseout="lists_change_color(this,false);" onClick="lists_customer_prefilter(<?php echo $customer['customerID']?>,'highlight'); $(this).blur(); return false;">
-          <?php if ($customer['visible'] != 1): ?><span style="color:#bbb"><?php endif; ?>
-          <?php if ($this->kga['conf']['showIDs'] == 1): ?><span class="ids"><?php echo $customer['customerID']?></span> <?php endif; echo $this->escape($customer['name'])?>
-          <?php if ($customer['visible'] != 1): ?></span><?php endif; ?>
-      </td>
-
-      <!-- annotation cell -->
-      <td nowrap class="annotation"></td>
-
-    </tr>     
-    <?php endforeach; ?>
-
-<?php if ($this->customers == '0'): ?>
+    <?php
+    if (!isset($this->customers) || $this->customers == '0')
+    {
+        ?>
         <tr>
-            <td colspan='3'>
-                <strong style="color:red"><?php echo $this->kga['lang']['noItems']?></strong>
+            <td nowrap colspan='3'>
+                <?php echo $this->error(); ?>
             </td>
         </tr>
-<?php endif; ?>
+        <?php
+    }
+    else
+    {
+        foreach ($this->customers as  $customer)
+        {
+            if (!$customer['visible']) {
+                continue;
+            }
+            ?>
 
+            <tr id="row_customer<?php echo $customer['customerID']?>" class="customer customer<?php echo $customer['customerID']?> <?php echo $this->cycle(array('odd','even'))->next()?>">
 
-    </tbody>  
+              <!-- option cell -->
+              <td nowrap class="option">
+                <?php if ($this->kga['user'] && $this->kga['user']['status'] != 2): ?>
+                <a href ="#" onClick="editSubject('customer',<?php echo $customer['customerID']?>); $(this).blur(); return false;">
+                  <img src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/edit2.gif' width='13' height='13' alt='<?php echo $this->kga['lang']['edit']?>' title='<?php echo $this->kga['lang']['edit']?>' border='0' />
+                </a>
+                <?php else: ?>
+                <a href ="#" onClick="lists_update_filter('customer',<?php echo $customer['customerID']?>); $(this).blur(); return false;">
+                  <img src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/filter.png' width='13' height='13' alt='<?php echo $this->kga['lang']['filter']?>' title='<?php echo $this->kga['lang']['filter']?>' border='0' />
+                </a>
+                <?php endif; ?>
+              </td>
+
+              <!-- name cell -->
+              <td width="100%" class="clients" onmouseover="lists_change_color(this,true);" onmouseout="lists_change_color(this,false);" onClick="lists_customer_prefilter(<?php echo $customer['customerID']?>,'highlight'); $(this).blur(); return false;">
+                  <?php if ($customer['visible'] != 1): ?><span style="color:#bbb"><?php endif; ?>
+                  <?php if ($this->kga['conf']['showIDs'] == 1): ?><span class="ids"><?php echo $customer['customerID']?></span> <?php endif; echo $this->escape($customer['name'])?>
+                  <?php if ($customer['visible'] != 1): ?></span><?php endif; ?>
+              </td>
+
+              <!-- annotation cell -->
+              <td nowrap class="annotation"></td>
+            </tr>
+            <?php
+        }
+    }
+    ?>
+    </tbody>
 </table>  
--- core/templates/scripts/lists/projects.php
+++ core/templates/scripts/lists/projects.php
@@ -1,71 +1,79 @@
 <table>
-
   <tbody>
+ <?php
+    if (!isset($this->projects) || $this->projects == '0')
+    {
+        ?>
+        <tr>
+            <td nowrap colspan='3'>
+                <?php echo $this->error(); ?>
+            </td>
+        </tr>
+        <?php
+    }
+    else
+    {
+        foreach ($this->projects as $project)
+        {
+            if (!$project['visible']) {
+                continue;
+            }
+            ?>
+            <tr id="row_project<?php echo $project['projectID']?>" class="project customer<?php echo $project['customerID']?> <?php echo $this->cycle(array('odd','even'))->next()?>" >
 
-<?php foreach ($this->projects as $project):
-  if (!$project['visible']) continue;
-?>
-    <tr id="row_project<?php echo $project['projectID']?>" class="project customer<?php echo $project['customerID']?> <?php echo $this->cycle(array('odd','even'))->next()?>" >
-    
-      <td nowrap class="option">
+              <td nowrap class="option">
 
-        <?php if ($this->kga['user'] && $this->kga['user']['status'] != 2): ?>
-        <a href ="#" onClick="editSubject('project',<?php echo $project['projectID']?>); $(this).blur(); return false;">
-          <img src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/edit2.gif' width='13' height='13' alt='<?php echo $this->kga['lang']['edit']?>' title='<?php echo $this->kga['lang']['edit']?> (ID:<?php echo $project['projectID']?>)' border='0' />
-        </a>
-        <?php endif; ?>
-        <a href ="#" onClick="lists_update_filter('project',<?php echo $project['projectID']?>); $(this).blur(); return false;">
-          <img src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/filter.png' width='13' height='13' alt='<?php echo $this->kga['lang']['filter']?>' title='<?php echo $this->kga['lang']['filter']?>' border='0' />
-        </a>
-        <a href ="#" class="preselect" onClick="buzzer_preselect('project',<?php echo $project['projectID']?>,'<?php echo $this->jsEscape($project['name'])?>',<?php echo $project['customerID']?>,'<?php echo $this->jsEscape($project['customerName'])?>'); lists_reload('activity'); return false;" id="ps<?php echo $project['projectID']?>">
-          <img src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/preselect_off.png' width='13' height='13' alt='<?php echo $this->kga['lang']['select']?>' title='<?php echo $this->kga['lang']['select']?> (ID:<?php echo $project['projectID']?>)' border='0' />
-        </a>
-    </td>
-
-    <td width="100%" class="projects" onmouseover="lists_change_color(this,true);" onmouseout="lists_change_color(this,false);" onClick="buzzer_preselect('project',<?php echo $project['projectID']?>,'<?php echo $this->jsEscape($project['name'])?>',<?php echo $project['customerID']?>,'<?php echo $this->jsEscape($project['customerName'])?>'); lists_reload('activity'); return false;">
-        <?php if ($project['visible'] != 1): ?><span style="color:#bbb"><?php endif; ?>
-        <?php if ($this->kga['conf']['flip_project_display']): ?>
-            <?php if ($this->kga['conf']['showIDs'] == 1): ?>
-              <span class="ids"><?php echo $project['projectID']?></span>
-            <?php endif; ?>
-            <span class="lighter"><?php echo $this->escape($this->truncate($project['customerName'],30,'...'))?>:</span> <?php echo $this->escape($project['name']) ?>
-        <?php else: ?>
-            <?php if ($this->kga['conf']['project_comment_flag'] == 1): ?>
-                <?php if ($this->kga['conf']['showIDs'] == 1): ?>
-                  <span class="ids"><?php echo $project['projectID']?></span>
+                <?php if ($this->kga['user'] && $this->kga['user']['status'] != 2): ?>
+                <a href ="#" onClick="editSubject('project',<?php echo $project['projectID']?>); $(this).blur(); return false;">
+                  <img src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/edit2.gif' width='13' height='13' alt='<?php echo $this->kga['lang']['edit']?>' title='<?php echo $this->kga['lang']['edit']?> (ID:<?php echo $project['projectID']?>)' border='0' />
+                </a>
                 <?php endif; ?>
-                <?php echo $this->escape($projects['name'])?>
-                <span class="lighter">
-                <?php if ($project['comment']): ?>
-                  (<?php echo $this->escape($this->truncate($project['comment'],30,'...')) ?>)
+                <a href ="#" onClick="lists_update_filter('project',<?php echo $project['projectID']?>); $(this).blur(); return false;">
+                  <img src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/filter.png' width='13' height='13' alt='<?php echo $this->kga['lang']['filter']?>' title='<?php echo $this->kga['lang']['filter']?>' border='0' />
+                </a>
+                <a href ="#" class="preselect" onClick="buzzer_preselect('project',<?php echo $project['projectID']?>,'<?php echo $this->jsEscape($project['name'])?>',<?php echo $project['customerID']?>,'<?php echo $this->jsEscape($project['customerName'])?>'); lists_reload('activity'); return false;" id="ps<?php echo $project['projectID']?>">
+                  <img src='../skins/<?php echo $this->escape($this->kga['conf']['skin'])?>/grfx/preselect_off.png' width='13' height='13' alt='<?php echo $this->kga['lang']['select']?>' title='<?php echo $this->kga['lang']['select']?> (ID:<?php echo $project['projectID']?>)' border='0' />
+                </a>
+            </td>
+
+            <td width="100%" class="projects" onmouseover="lists_change_color(this,true);" onmouseout="lists_change_color(this,false);" onClick="buzzer_preselect('project',<?php echo $project['projectID']?>,'<?php echo $this->jsEscape($project['name'])?>',<?php echo $project['customerID']?>,'<?php echo $this->jsEscape($project['customerName'])?>'); lists_reload('activity'); return false;">
+                <?php if ($project['visible'] != 1): ?><span style="color:#bbb"><?php endif; ?>
+                <?php if ($this->kga['conf']['flip_project_display']): ?>
+                    <?php if ($this->kga['conf']['showIDs'] == 1): ?>
+                      <span class="ids"><?php echo $project['projectID']?></span>
+                    <?php endif; ?>
+                    <span class="lighter"><?php echo $this->escape($this->truncate($project['customerName'],30,'...'))?>:</span> <?php echo $this->escape($project['name']) ?>
                 <?php else: ?>
-                  <span class="lighter">(<?php echo $this->escape($project['customerName'])?>)</span>
+                    <?php if ($this->kga['conf']['project_comment_flag'] == 1): ?>
+                        <?php if ($this->kga['conf']['showIDs'] == 1): ?>
+                          <span class="ids"><?php echo $project['projectID']?></span>
+                        <?php endif; ?>
+                        <?php echo $this->escape($projects['name'])?>
+                        <span class="lighter">
+                        <?php if ($project['comment']): ?>
+                          (<?php echo $this->escape($this->truncate($project['comment'],30,'...')) ?>)
+                        <?php else: ?>
+                          <span class="lighter">(<?php echo $this->escape($project['customerName'])?>)</span>
+                        <?php endif; ?>
+                        </span>
+                    <?php else: ?>
+                        <?php if ($this->kga['conf']['showIDs'] == 1): ?>
+                          <span class="ids"><?php echo $project['projectID']?></span>
+                        <?php endif; ?>
+                        <?php echo $this->escape($project['name'])?>
+                        <span class="lighter">(<?php echo $this->escape($this->truncate($project['customerName'],30,'...')) ?>)</span>
+                    <?php endif; ?>
                 <?php endif; ?>
-                </span>
-            <?php else: ?>
-                <?php if ($this->kga['conf']['showIDs'] == 1): ?>
-                  <span class="ids"><?php echo $project['projectID']?></span>
-                <?php endif; ?>
-                <?php echo $this->escape($project['name'])?>
-                <span class="lighter">(<?php echo $this->escape($this->truncate($project['customerName'],30,'...')) ?>)</span>
-            <?php endif; ?>
-        <?php endif; ?>
-        <?php if ($project['visible'] != 1): ?></span><?php endif; ?>
-    </td>
+                <?php if ($project['visible'] != 1): ?></span><?php endif; ?>
+            </td>
 
 
-    <td nowrap class="annotation"></td>
+            <td nowrap class="annotation"></td>
 
-  </tr>   
-<?php endforeach; ?>
-
-<?php if ($this->projects == '0'): ?>
-  <tr>
-      <td nowrap colspan='3'>
-          <strong style="color:red"><?php echo $this->kga['lang']['noItems']?></strong>
-      </td>
-  </tr>
-<?php endif; ?>
-
-  </tbody>  
+          </tr>
+        <?php
+        }
+    }
+    ?>
+  </tbody>
 </table>  
