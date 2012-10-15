e8ef6241a62778afb6a6ce4fa34a49a9dbc2886b
kevinpapst <kpapst@gmx.net>
2012-10-15 21:04:15 +0200

    code format

--- core/templates/scripts/core/main.php
+++ core/templates/scripts/core/main.php
@@ -310,15 +310,17 @@
     
     <div id="gui">
     	<div id="extdiv_0" class="ext ki_timesheet"></div>
-    	
-<?php for ($i = 0; $i < count($this->extensions); $i++): 
-  if ($this->extensions[$i] != "ki_timesheet"): ?>
-		<div id="extdiv_<?php echo $i+1; ?>" class="ext <?php echo $this->extensions[$i]['key']?>" style="display:none;"></div>
-<?php
-  endif;
-endfor;
-?>
-
+        <?php
+            for ($i = 0; $i < count($this->extensions); $i++)
+            {
+                if ($this->extensions[$i] != "ki_timesheet")
+                {
+                    ?>
+                    <div id="extdiv_<?php echo $i+1; ?>" class="ext <?php echo $this->extensions[$i]['key']?>" style="display:none;"></div>
+                    <?php
+                }
+            }
+        ?>
     </div>
 
 <div class="lists" style="display:none">
