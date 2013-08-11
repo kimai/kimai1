4a8f32bc0dbbfb2f51a2176449dfa6a9aecdea7b
Kevin Papst <kpapst@gmx.net>
2013-08-11 20:23:22 +0200

    added skin support to kimai view

--- core/libraries/Kimai/View.php
+++ core/libraries/Kimai/View.php
@@ -6,11 +6,26 @@
  */
 class Kimai_View extends Zend_View
 {
+
     public function init()
     {
         $this->setBasePath(APPLICATION_PATH . '/templates');
         $this->addHelperPath(APPLICATION_PATH . '/templates/helpers', 'Zend_View_Helper');
 
+        if (isset($GLOBALS['kga']['conf']['skin'])) {
+            $this->enableSkinSupport($GLOBALS['kga']['conf']['skin']);
+        }
+
         parent::init();
     }
+
+    public function enableSkinSupport($skin)
+    {
+        // allow skin specific view helper
+        $helperPath = APPLICATION_PATH . '/templates/helpers/' . ucfirst($skin) . '/';
+        if(file_exists($helperPath) && is_dir($helperPath)) {
+            $this->addHelperPath($helperPath, 'Zend_View_Helper_'.ucfirst($skin).'_');
+        }
+    }
+
 }
