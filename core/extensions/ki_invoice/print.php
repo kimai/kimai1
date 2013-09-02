26ab32c867dc2c28443c97d9aa9783688eb80de7
Kevin Papst <kpapst@gmx.net>
2013-09-02 16:23:36 +0200

    division by zero

--- core/extensions/ki_invoice/print.php
+++ core/extensions/ki_invoice/print.php
@@ -164,7 +164,13 @@ if (isset($_REQUEST['round'])) {
 		// Write a logfile entry for each value that is rounded.
 		Logger::logfile("Round ".  $invoiceArray[$time_index]['hour'] . " to " . $rounded . " with ".  $round);
 
-		$rate = RoundValue($invoiceArray[$time_index]['amount']/$invoiceArray[$time_index]['hour'],0.05);
+        if ($invoiceArray[$time_index]['hour'] == 0) {
+            // make sure we do not raise a "divison by zero" - there might be entries with the zero seconds
+            $rate = 0;
+        } else {
+		    $rate = RoundValue($invoiceArray[$time_index]['amount']/$invoiceArray[$time_index]['hour'],0.05);
+        }
+
 		$invoiceArray[$time_index]['hour'] = $rounded;
 		$invoiceArray[$time_index]['amount'] = $invoiceArray[$time_index]['hour']*$rate;
 		$time_index++;
