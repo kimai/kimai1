f9132196765a5fdd927e5bf46c91212a76b1a199
Kevin Papst <kpapst@gmx.net>
2013-09-04 21:24:37 +0200

    fixed refresh ids

--- core/extensions/ki_adminpanel/js/ap_func.js
+++ core/extensions/ki_adminpanel/js/ap_func.js
@@ -319,16 +319,16 @@ function adminPanel_extension_refreshSubtab(tab) {
     $.post(adminPanel_extension_path + "processor.php", options, 
     function(data) {
         switch(tab) {
-            case "users":  	target = "#adminPanel_extension_s1"; break
-            case "groups":  	target = "#adminPanel_extension_s2"; break
-            case "status":  target = "#adminPanel_extension_s3"; break
-            case "advanced":  	target = "#adminPanel_extension_s4"; break
-            case "database":   	target = "#adminPanel_extension_s5"; break
-            case "customers":  	target = "#adminPanel_extension_s6"; break
-            case "projects": 	target = "#adminPanel_extension_s7"; break
-            case "activities": 	target = "#adminPanel_extension_s8"; break
-            case "globalRoles":  target = "#adminPanel_extension_s9"; break
-            case "membershipRoles": target = "#adminPanel_extension_s10"; break
+            case "customers":  	    target = "#adminPanel_extension_s1"; break
+            case "projects": 	    target = "#adminPanel_extension_s2"; break
+            case "activities": 	    target = "#adminPanel_extension_s3"; break
+            case "users":  	        target = "#adminPanel_extension_s4"; break
+            case "groups":  	    target = "#adminPanel_extension_s5"; break
+            case "globalRoles":     target = "#adminPanel_extension_s6"; break
+            case "membershipRoles": target = "#adminPanel_extension_s7"; break
+            case "status":          target = "#adminPanel_extension_s8"; break
+            case "advanced":  	    target = "#adminPanel_extension_s9"; break
+            case "database":   	    target = "#adminPanel_extension_s10"; break
         }
         $(target).html(data);
     });
