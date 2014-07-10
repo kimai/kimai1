ade61940662e4acba0f756569a0706536845b493
albertlast <mathiaspapealbert@hotmail.com>
2014-07-10 19:37:50 +0200

    Install requirements fix

--- core/installer/installscript.js
+++ core/installer/installscript.js
@@ -114,11 +114,13 @@ function resetRequirementsIndicators() {
     $('div.sp_phpversion').removeClass("fail");
     $('div.sp_magicquotes').removeClass("fail");
     $('div.sp_mysql').removeClass("fail");
+    $('div.sp_pgsql').removeClass("fail");
     $('div.sp_memory').removeClass("fail");
     
     $('div.sp_phpversion').addClass("ok");
     $('div.sp_magicquotes').addClass("ok");
     $('div.sp_mysql').addClass("ok");
+    $('div.sp_pgsql').addClass("ok");
     $('div.sp_memory').addClass("ok");
 }
 
