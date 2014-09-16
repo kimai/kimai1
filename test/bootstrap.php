d9201917e88aed228f566ffc83dd2783da6e6b1c
Andreas Heigl <andreas@heigl.org>
2014-09-16 08:43:09 +0200

    Adds a testing framework
    This commit adds some phpunit-files to make testing easier as well as
    adding some files to enable testing via TravisCI.

---
+++ test/bootstrap.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author heiglandreas
+ */
+
+// TODO: check include path
+ini_set ( 'date.timezone', 'Europe/Berlin' );
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+
+ini_set('include_path', ini_get('include_path')
+                        . PATH_SEPARATOR . __DIR__ . '/library'
+                        . PATH_SEPARATOR . __DIR__ . '/../core/libraries'
+);
+
+class UnitTestHelper
+{
+    /**
+     * Access protected or private methods
+     *
+     * use the following code to access any protected or private class method
+     * $obj = new MyClass();
+     * $method = UnitTestHelper::getMethod($obj, 'nameOfMethod');
+     * $result = $method->invoke($obj,'param1','param2');
+     *
+     * @param Object|string $obj
+     * @param string $name
+     *
+     * @return method
+     */
+    public static function getMethod($obj, $name) {
+        $class = new ReflectionClass($obj);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
+}
