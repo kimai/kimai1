0094f9e5636e9188555f9a8f07dd3da06e2f49aa
Jess Taylor <finalfantasyaddict@gmail.com>
2015-01-08 16:04:04 +0900

    jp.php
    Japanese localization files for Kimai Time Tracking

---
+++ core/language/jp.php
@@ -0,0 +1,468 @@
+<?php
+/**
+ * このファイルは
+ * Kimai - Open Source Time Tracking // http://www.kimai.org
+ * (c) 2006-2009 Kimai-Development-Team
+ * の一部となります。
+ *
+ * Kimai is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; Version 3, 29 June 2007
+ *
+ * Kimai is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kimai; If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+/**
+ * japanese
+ *
+ */
+return array(
+"countryCode"       => "jp-JP",
+"preferences"       => "設定",
+"total"		        => "合計",
+"monatzeit"         => "月",
+"heute"             => "今日",
+"gestern"           => "昨日",
+"autoOn"            => "オート オン",
+"autoOff"           => "オート オフ",
+"datum"             => "日付",
+"in"                => "入",
+"out"               => "出",
+"time"              => "時：分",
+"timelabel"         => "時間",
+"durationlabel"     => "期間",
+"knd"               => "客様",
+"pct"               => "プロジェクト",
+"evt"               => "タスク",
+"knds"              => "客様",
+"pcts"              => "プロジェクト",
+"evts"              => "タスク",
+"admin"             => "管理",
+"rowlimit"          => "最大行",
+"breiteZeitspalte"  => "時間列の幅",
+"breiteUhrspalte"   => "時計列の幅",
+"breiteDatumspalte" => "日付列の幅",
+"def_pctID"         => "既定プロジェクトID",
+"def_kndID"         => "既定客様ID",
+"def_evtID"         => "既定イベントID",
+"recommended"       => "おすすめ",
+"submit"            => "OK",
+"cancel"            => "キャンセル",
+"edit"              => "登録編集",
+"construction"      => "工事中...",
+"about"             => "について",			// This needs to go after Kimai. 　　ie. Kimaiについて
+"hours"             => "時間",
+"mins"              => "分",
+"secs"              => "秒",
+"skin"              => "スキン",
+"erase"             => "登録されてるデータを削除する",
+"new_knd"           => "客様追加",
+"new_pct"           => "プロジェクト追加",
+"new_evt"           => "タスク追加",
+"day_summary"       => "時間概要",
+"plzChoose"         => "選択してください",
+"noEntries"         => "指定してる期間にエントリーがない...",
+"recordAgain"       => "このイベントをもう一度登録する",
+"comment"           => "コメント",
+"zlocation"         => "場所",
+"trackingnr"        => "追跡番号",
+"cookiewarning"     => "進む前にクッキー許可してください",
+"JSwarning"         => "JavaScriptは活性化必要がある",
+"designation"       => "指定",
+"banned"            => "禁止されてる(アカウントがロックされている)",
+"tooManyLogins"     => "ログインは間違い過ぎた - ユーザーは禁止される",
+"accessDenied"      => "アクセス禁止！",
+"wrongPass"         => "ユーザー名かパスワードは間違っている",
+"tryAgain"          => "もう一度入力してください / パスワード忘れた？",
+"back"              => "ログイン画面に戻る",
+"bye"               => "バイ！",
+"login"             => "ログイン",
+"logout"            => "ログアウト",
+"loggedon"          => "ログインされてるユーザー：",
+"username"          => "ユーザー名",
+"password"          => "パスワード",
+"newPassword"       => "新パスワード",
+"retypePassword"    => "パスワード再入力",
+"passwordsDontMatch"=> "入力しているパスワードは一致しない",
+"passwordTooShort"  => "パスワードは5桁以上ではない",
+"minLength"         => "5桁以上！",
+"update"            => "インストールされてるKimaiバージョンのデータベース構造のデータベースアップデートする必要があります<br />あぷデート中にデータがなくならないために登録されてるタスクをバックアップしたほうがよい。<br />",
+"updateNow"         => "データベースをすぐアップデートする！",
+"stop"              => "止まる!",
+"start"             => "スタート！",
+"setTimespace"      => "タイムスペースを設定する",   
+"now"               => "今",
+"specify"           => "リストを特定する",
+"add"               => "追加",
+"running"           => "実行中...",
+"company"           => "会社",
+
+"vat"               => "税金",
+"contactPerson"     => "間に合わせ先",
+
+"street"            => "道",
+"zipcode"           => "郵便番号",
+"city"              => "市",
+"telephon"          => "電話番号",
+"fax"               => "Fax",
+"mobilephone"       => "携帯電話",
+"mail"              => "メール",
+"homepage"          => "インターネット",
+"users"             => "ユーザー",
+"name"              => "名前",
+"alias"             => "ニックネーム",
+"adduser"           => "ユーザー追加",
+"addgroup"          => "グループ追加",
+"group"             => "クループ",
+"groups"            => "クループ",
+"advanced"          => "詳細",
+"database"          => "データベース",
+"options"           => "オプション",
+"status"            => "ステータス",
+"adminmail"         => "管理者のメールアドレス",
+"logintries"        => "ログイン不可になるまでのログイン試行数",
+"bantime"           => "ログイン不可時間（秒）",
+"lastdbbackup"      => "最新バックアップ",
+"runbackup"         => "今すぐバックアップ",
+"defaultgrp"        => "既定 (0)",
+"regusr"            => "一般ユーザー",
+"adminusr"          => "管理ユーザー",
+"editusr"           => "ユーザー編集",
+"editgrp"           => "グループ編集",
+"delgrp"            => "グループ削除",
+"delpct"            => "プロジェクト削除",
+"delknd"            => "客様削除",
+"delevt"            => "タスク削除",
+"switchusr"         => "このユーザーに変える",
+"backupusr"         => "このユーザーをバックアップする",
+"mailusr"           => "このユーザーにメールする",
+"delusr"            => "このユーザーを削除する",
+"bannedusr"         => "禁止されてるユーザー - クリックして再活性化させる",
+"activeusr"         => "活性化されてるユーザー - クリックして禁止させる",
+"changegrp"         => "グループを変える",
+"members"           => "会員",
+"groupleader"       => "グループ長",
+"nopassword"        => "パスワード設定してない！！！",
+"noItems"           => "登録されてるデータがない",
+"DBname"            => "データベース名",
+"autoselection"     => "自動選択",
+"quickdelete"       => "テーブル内の登録されてるデータを削除するオプション",
+"quickdeleteHide"   => "いいえ",
+"quickdeleteShow"   => "はい、確認ダイアログ無しで",
+"quickdeleteShowConfirm" => "はい、確認ダイアログ有りで",
+"tools"             => "ツール",
+"print"             => "印刷",
+"save"              => "保存",
+"export"            => "出力",
+"groupname"         => "グループ名",
+"help"              => "ヘルプ",
+"select"            => "選択",
+"hidedeletedusers"  => "削除されてるユーザーを非表示する",
+"showdeletedusers"  => "削除されてるユーザーを表示する",
+
+"month"             => "月",
+"day"               => "日",
+"year"              => "年",
+
+"lang"              => "言語",
+
+"ac"                => "自動継続されてるエントリー",
+"ac_error"          => "エラーが発生した自動継続されてるエントリー　（期間はかかり過ぎた）",
+"refundable_long"   => "返金　（社員に）",
+"refundable"        => "返金",
+"yes"               => "はい",
+"no"                => "いいえ",
+"comment_type"      => "コメントタイプ",
+"ctype0"            => "コメント",
+"ctype1"            => "おしらせ",
+"ctype2"            => "注意！",
+"hint"              => "ヒント:", 
+"usr_caution1"      => "",			// This line is not required in Japanese as it does not make grammatical sense to write anything before the subject <username>. The "If you change the user-name of" line has been added to the following line.
+"usr_caution2"      => "のユーザー名を変更したら、すぐに新ユーザー名に再ログインが必要です。",
+"timediff_warn"     => "「入」、「出」のフィールドに違う時間を入れなければならない！",
+"visibility"        => "可視性",
+"showitem"          => "見えるようにする",
+"hideitem"          => "見えないようにする",
+"checkUsername"     => "追加ボタンの左にあるフィールドにユーザー名を入れなければならない！",
+"checkGroupname"    => "追加ボタンの左にあるフィールドにグループ名を入れなければならない！",
+"clickToReload"     => "GUIをリロードするためにここにクリックしてください",
+"sure"              => "よろしいですか？",
+"clock"             => "時計",
+"currSum"           => "現在のタイムスペース概要",
+"admWarn"           => "あなたは管理者です。他の管理者しかステータス下がらせない",
+"flip_pct_display"  => "客様順番にプロジェクトテーブルを順位する",
+"pct_comment_flag"  => "プロジェクトの横にいつもコメントを表示する",
+"showIDs"           => "IDを表示する",
+"error"             => "エラーが発生しました！",
+"checkupdate"       => "アップデートを探す",
+
+"selectR"           => "記録するために選択したこと：",
+"selectKND"         => "客：",
+"selectPCT"         => "プ：",
+"selectEVT"         => "タ：",
+
+"testKND"           => "テスト-客様",
+"testPCT"           => "テスト-プロジェクト",
+"testEVT"           => "テスト",
+
+"budget"            => "予算",
+"rate"              => "レート (時給)",
+"rate_short"        => "レート",
+"default_rate"      => "既定レート",
+"my_rate"           => "私のレート",
+
+"wage"              => "賃金",
+
+"customerlogin"     => "客様ログイン",
+
+"expense"           => "経費",
+"designation"       => "指定",
+
+"cleared"           => "完了されてる",
+
+"atLeastOneGroup" => '少なくとも一つのグループを選択しなければならない',
+
+"noFading"          => "フェージング効果無しで",
+"user_list_hidden"  => "既定にユーザーリストを非表示する",
+
+'show_sensible_data' => 'デバッグエクステンションにセンシブルデータを表示する',			// What is sensible data?
+'show_update_warn' => 'アップデートの前に注意が面を非表示にする',
+'check_at_startup' => '新バージョンが利用できるようになったら、路銀ページにメッセージを表示する',
+'show_daySeperatorLines' => '違う日の間に区別線を表示する',
+'show_gabBreaks' => '完璧に連続していない登録されてるデータに線を表示する',
+'show_RecordAgain' => '全エントリーの前に　&#12300;再登録&#12301;　ボタンを表示する',
+'show_TrackingNr' => '追跡番号を編集可能にする',
+'currency_name' => '使用する通貨名',
+'currency_sign' => '使用する通貨文字',
+'currency_first' => '値の前に通貨名を表示する (e.g. &yen; 10,000)',
+'display_date_format' => '期間の日付フォーマット (<a href="http://php.net/manual/ja/function.strftime.php" target="_blank">strftime()の表記法</a>)',
+'display_currentDate_format' => '期間の下記にあるフィールドの日付フォーマット (<a href="http://php.net/manual/ja/function.strftime.php" target="_blank">strftime()の表記法</a>)',
+'table_date_format' => 'テーブルの日付フォーマット (<a href="http://php.net/manual/ja/function.strftime.php" target="_blank">strftime()の表記法</a>)',
+'round_time' => '時間を ',
+'round_time_minute' => ' 分に丸める',
+'logged_in_as' => '現在ユーザー：',
+
+'decimal_separator'  => '小数点の記号',
+
+'view_filter' => 'ビューフィルター',
+'unassigned' => '割り当てられていない',
+'all_events' => '&lt;&lt;all&gt;&gt;',
+
+'durationWithSeconds' => '期間は「秒」までに表示する',
+
+'multiplier' => '乗数',
+
+'dateAndTimeHelp' => '<strong>時間と日付は下記の通りにしなければならない：</strong><br />
+            Dates: 「日日.月月.年年年年」<br />
+            Times: 「時時:分分:秒秒」 or 「時時:分分」 or 「時時」　だけ。 省略されてる部分は「0」に設定する',
+
+'timezone' => '時間帯',
+'defaultTimezone' => '新ユーザーと客様の時間帯',
+
+'hideClearedEntries' => '削除されてるエントリーを非表示',
+
+'general' => '一般',
+'address' => '住所',
+'contact' => '連絡先',
+
+'internalProject' => '社内プロジェクト',
+
+'exactSums' => '丸めるの値合計ではなくて、正確な合計を使用する',
+
+'securityWarning' => 'セキュリティ注意',
+'installerWarningHeadline' => 'インストールフォルダーまだ存在しています！',
+'installerWarningText' => 'Kimaiをリーインストールをできないため、インストールフォルダーを削除してください',
+
+'change' => '変更する',
+
+'sublistAnnotations' => 'サブリストの注釈',
+
+'TimeDateInputError' => '入力した時間か日付は無効です',
+
+'StartTimeBeforeEndTime' => '開始時刻は終了時間の前に入力しなければならない',
+
+
+
+'credits' => <<<EOF
+        Torsten H&ouml;ltge, Severin Leonhardt, Kevin Papst, Oleg Britvin, Martin Klemkow and others...<br />
+        
+        <br />
+        
+        <strong>This program is free software and published 'AS IS' without any kind of warranty</strong> under the terms of the
+        <a href="../COPYING">GPL Version 3</a>. 
+        
+        <br /><br />
+         
+        <a href='http://www.kimai.org' target='_blank'>公式サイト</a> | 
+        <a href='http://forum.kimai.org/' target='_blank'>フォーラム</a> |
+        <a href='http://sourceforge.net/projects/kimai/' target='_blank'>Sourceforge プロジェクトページ</a> | 
+        <a href='http://sourceforge.net/projects/kimai/develop' target='_blank'>Subversion リポジトリー</a> |
+        <a href='http://www.kimai.org/en/download.html' target='_blank'>ダウンロード</a>
+        
+        <br /><br />
+
+        If you do any alternations we would appreciate a posting on the <a href="http://forum.kimai.org" title="Kimai Forum">forum</a>. You'll find a special board "<a href="http://forum.kimai.org/index.php?board=10.0" title="Extensions / Friendly Hacks">Friendly Hacks</a>" there. Latest SVN Version can be downloaded <a href="http://kimai.svn.sourceforge.net/viewvc/kimai.tar.gz?view=tar">here</a> (download for testing purposes only!).
+        
+        <br /><br />
+        
+        Please support further development of Kimai with a <a href="http://www.kimai.org/en/donate.html">donation</a> or by extending its <a href="http://kimai.svn.sourceforge.net/viewvc/kimai/kimai/core/language/">language library</a>.
+
+        If you find bugs or have ideas for features, feel free to contact us through the forum. Or even better: create a <a href="http://sourceforge.net/tracker/?group_id=186214">ticket</a>!<br /><br />
+
+        <strong>Further thanks for support to:</strong> Vasilis van Gemert, Maximilian Kern, Enrico Ties, Thomas Wensing, John Resig, Kelvin Luck, Urs Gerig, Willem van Gemert,
+        Torben Boe and HamBug Studios, Klaus Franken, Chris (Urban Willi), Andreas Berndt, Niels Hoffmann, G&uuml;nter Hengsbach, Paul Brand, Joaqu&iacute;n G. de la Zerda,
+        Allesandro Bertoldo, Jos&eacute; Ricardo Cardoso.<br /><br />
+
+        <strong>Special thanks</strong> to all beta testers, which are way to many to mention them all - you know who you are! ;)<br /><br />
+
+        The invoice extension uses the PclZip Library, which can be found at <a href="http://www.phpconcept.net" rel="nofollow">http://www.phpconcept.net</a>.<br/><br/>
+
+        Version 0.8 was emerged with the kind support of RRZE (Regionales Rechenzentrum Erlangen).<br /><br />
+        <br />
+EOF
+,
+
+
+
+'months'         => array("1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"),
+'months_short'   => array("1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"),
+'weekdays'       => array("日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"),
+'weekdays_short' => array("金","月","火","水","木","金","土"),
+
+'errors' => array (
+  0 => array ('hdl' => "データベースを接続できなかった！",
+              'txt' => "データベースを実行させて、PDOコネクターは活性化をさせて、'includes'フォルダーの中にある'autoconf.php'のエントリーは正しいと確認してください"),
+
+  1 => array ('hdl' => "インストール中にエラーが発生しました！",
+              'txt' => 'Kimaiは同じ名前のテーブルが存在してるため、必要なテー部を作れない。<br/>
+現在のインストールをアップデートしたい場合、<a href="../index.php">here</a>をクリックしてください<br/>
+Kimaiをクリーンインストールができるために、別の接頭辞を選択するか、この接頭辞を使ってるテーブルを削除してください')
+  ),
+
+'updater' => array (
+0        => "はい、Kimaiデータベースのバックアップを持っています。アップデートを進んでください！",
+10       => "凡例：",
+20       => "クエリーを正常に実行されました",
+30       => "クエリーはエラーを発生しました - 無関係の可能性は高い",
+40       => "クエリーは致命的なエラーを発生しました",
+50       => "バックアップログ",
+60       => "バックアップを失敗しました - アップデート中止されました！",
+70       => "アップデートログ：",
+80       => "バックアップを作成の上に、データバースをアップデートできました",
+90       => "クエリーを処理されました",
+100      => "致命的なエラーの影響でデータベースアップデートは失敗しました。Kimaiが正常に動作しない可能性のため、データベースを再作成できるバックアップを作成しました",
+110      => "新パスワード",
+120      => "下記に全ユーザーの新パスワードを表示しています",
+130      => "「autoconf.php」に書き込むことはできませんでした",
+140      => "「autoconf.php」をアップデートしてください",
+'timezone' => 'Kimaiは時間帯をサポートしています。　既定時間帯を選択してください。全ユーザーと客様に使います。ユーザーは時間帯を変更可能です',
+),
+
+'backup' => array(
+0        => "このバックアップ復元ユーティリティはまだテスティングされていない状態。ご自身の責任で使用してください！",
+1        => "利用可能バックアップ：",
+2        => "復元",
+3        => "削除",
+4        => "OK",
+5        => "バックアップを同時に*一つずつ*しかできない！",
+6        => "",			// This line is not required in Japanese as it does not make grammatical sense to write anything before the subject <backup date>. The "Backup from" line has been added to the following line.
+7        => "のバックアップから復元しました",
+8        => "バックアップを作成する",
+9        => "このバックアップはスナップショットです！Kimaiデータに保存しているデータベースを同時に保存しています。本当のバックアップでないので別の場所に本当のバックアップを作成してください"),
+
+
+'xp_ext' => array(
+	
+"print_hint"      		   => "「OK」をクリックすると、印刷できるテーブルを表示します",
+"dl_hint"         		   => "「OK」をクリックすると、出力ファイルをダウンロードします",
+                              
+"tab_filter"	 		   => "フィルター",
+"tab_location"	 		   => "場所",
+"tab_timeformat"	 	   => "時間フォーマット",
+                              
+"cleared_all"	 		   => "全部",
+"cleared_cleared" 		   => "完了のみ",
+"cleared_open"	 		   => "未完了のみ",
+"stdrd_location"  		   => "標準の場所",
+"timeformat"	     	   => "時間フォーマット",
+"dateformat"			   => "日付フォーマット",
+
+"filter"	 		       => "フィルター",
+
+"refundable_all"	 	   => "全部",
+"refundable_refundable"    => "返金のみ",
+"refundable_not_refundable"=> "返金不可のみ",
+                              
+"print_comment"   		   => "コメントを印刷する",
+"print_summary"   		   => "概要を印刷する",
+"create_bookmarks"		   => "ブックマークを作成する",
+"download_pdf"    		   => "PDFにしてダウンロード",
+"pdf_format"               => "ファイルフォーマット",
+"export_pdf"               => "テーブル",
+"export_pdf2"              => "リスト",
+                              
+"column_delimiter"		   => "列の区切り文字",
+"quote_char"      		   => "フィールドの境界文字",
+                              
+"exportCSV"         	   => "CSV 出力",
+"exportPDF"         	   => "PDF 出力",
+"exportXLS"         	   => "Excel 出力",
+"print"         	       => "印刷",
+"export_timeformat_help"   => "時間フォーマットのヘルプ",
+
+'subtotal'              => '小計',
+'finalamount'           => '合計',
+'pdf_headline'          => '労働時間と経費',
+'pdf_headline_only_times'   => '労働時間一覧',
+'pdf_headline_only_expenses'=> '経費一覧',
+'costs'                 => '費用',
+'expense'               => '経費',
+'summary'               => '概要',
+'full_list'             => '全リスト',
+'page'                  => 'ページ',
+'by'                    => 'by',			// What is this for?
+'done_by'               => 'done by',			// What is this for?
+'duration'              => '存続',
+'customer_new_page'     => '客様ごとに新ページ',
+
+'times_and_expenses' => '時間と経費',
+'times' => '時間',
+'expenses' => '経費',
+
+'reverse_order' => '古いデータから並べ替え',
+'time_period'  => '期間',
+
+'duration_unit' => 'h',
+
+),
+
+
+
+'ext_invoice' => array(
+	
+"invoiceTitle" 			=> "請求書作成",
+"invoiceProject" 		=> "プロジェクト:",
+"invoiceTimePeriod" 	=> "期間:",
+"invoiceOptionShort" 	=> "小請求書",
+"invoiceOptionRound" 	=> "概時間",
+"invoiceButton" 		=> "作成",
+'noData'                => 'プロジェクトと期間のために登録されてるデータがない',
+'defaultVat'        => '規定税金'
+),
+
+'ext_budget' => array(
+  'unusedBudget' => '未使用'
+),
+
+'extensions' => array()
+
+);
+
+
+?>
