219d8ceef81d1d0e5892a4b9cdb2d6f05d87ed77
Simon Schaufelberger <simonschaufi@googlemail.com>
2015-05-24 14:58:34 +0200

    FIX: recalculation of table header Adjust column widths so that it aligns properly with the header Optimize jQuery selectors Fix: charset of rendered printable table

--- core/extensions/ki_export/css/tables.css.php
+++ core/extensions/ki_export/css/tables.css.php
@@ -60,69 +60,64 @@ div.ki_export tr.odd td {
 }
 
 
-div#xptable tr.odd td.comment,
-div#xptable tr.odd td.trackingNumber,
-div#xptable tr.odd td.location {
+#xptable tr.odd td.comment,
+#xptable tr.odd td.trackingNumber,
+#xptable tr.odd td.location {
     background: #CFE3F5;
 }
 
-div#xptable tr.even td.comment,
-div#xptable tr.even td.trackingNumber,
-div#xptable tr.even td.location {
+#xptable tr.even td.comment,
+#xptable tr.even td.trackingNumber,
+#xptable tr.even td.location {
     background: #ECF7FD;
 }
-div#xptable tr.even.expense td {
+#xptable tr.even.expense td {
     background: #E5FFBF;
 }
 
-div#xptable tr.odd.expense td {
+#xptable tr.odd.expense td {
     background: #DCF4B7;
 }
 
-div#xptable tr.odd.expense td.comment,
-div#xptable tr.odd.expense td.trackingNumber,
-div#xptable tr.odd.expense td.location {
+#xptable tr.odd.expense td.comment,
+#xptable tr.odd.expense td.trackingNumber,
+#xptable tr.odd.expense td.location {
     background: #A3F1A6;
 }
 
-div#xptable tr.even.expense td.comment,
-div#xptable tr.even.expense td.trackingNumber,
-div#xptable tr.even.expense td.location {
+#xptable tr.even.expense td.comment,
+#xptable tr.even.expense td.trackingNumber,
+#xptable tr.even.expense td.location {
     background: #C8F1CC;
 }
 
-
-
-div#xptable tr.odd td.cleared {
+#xptable tr.odd td.cleared {
     background: #6E7882;
 }
 
-div#xptable tr.even td.cleared {
+#xptable tr.even td.cleared {
     background: #86939F;
 }
 
-
-div#xptable tr td.time {
+#xptable tr td.time {
     border-bottom:1px dotted white;;
 }
 
-div#xptable tr.even td.time {
+#xptable tr.even td.time {
     background: #A4E7A5;
 }
 
-div#xptable tr.odd td.time {
+#xptable tr.odd td.time {
     background: #64BF61;
 }
 
-div#xptable tr.active td.time {
+#xptable tr.active td.time {
     background: #F00;
 }
 
-div#xptable tr.expense td.time {
+#xptable tr.expense td.time {
     background-image:url('<?php echo $schraff3; ?>');
 }
-
-
 
 div.ki_export tr td.option,
 div.ki_export tr td.date,
@@ -133,7 +128,7 @@ div.ki_export tr td.wage {
     text-align:center;
 }
 
-div.ki_export>div#xp>div#xptable>table>tbody>tr>td.cleared {
+div.ki_export>div#xp>#xptable>table>tbody>tr>td.cleared {
     border-right: none;
 }
 
@@ -142,73 +137,61 @@ div.ki_export>div#xp>div#xptable>table>tbody>tr>td.cleared {
 }
 
 #export_head td.option,
-#xp td.option
-{
+#xp td.option {
     width:70px;
 }
 
 #export_head td.date,
-#xp td.date
-{
+#xp td.date {
     width:50px;
 }
 
 #export_head td.time,
-#xp td.time
-{
+#xp td.time {
     width:40px;
 }
 
 #export_head td.dec_time,
-#xp td.dec_time
-{
+#xp td.dec_time {
     width:40px;
 }
 
-#export_head td.cash
-{
+#export_head td.cash {
     width: 90px;
 }
 
-#xp td.budget
-{
+#xp td.budget {
     width:50px;
 }
 
-#xp td.status
-{
+#xp td.status {
     width:50px;
 }
 
-#xp td.billable
-{
-    width:40px;
+#xp td.billable {
+    min-width:100px;
 }
 
-#xp td.approved
-{
+#xp td.approved {
     width:50px;
 }
 
-
-#xp td.rate
-{
+#xp td.rate {
     width:50px;
 }
 
-#xp td.wage
-{
+#xp td.wage {
     width:50px;
 }
 
-#export_head td { white-space:nowrap; }
-
+#export_head td {
+	white-space:nowrap;
+}
 
 #export_head td.from,
 #export_head td.to,
 #xp td.from,
-#xp td.to
-{
+#xp td.to {
     width:50px;
 }
 
@@ -225,12 +208,11 @@ div.ki_export>div#xp>div#xptable>table>tbody>tr>td.cleared {
 }
 
 #xp td.comment {
-    width:40px;
+	min-width:80px;
 }
 
-
 #xp td.description {
-    width:120px;
+	min-width:120px;
 }
 
 #xp td.location {
@@ -238,58 +220,45 @@ div.ki_export>div#xp>div#xptable>table>tbody>tr>td.cleared {
 }
 
 #xp td.trackingNumber {
-    width:40px;
+    min-width:80px;
 }
 
 div.ki_export table tr.odd td.disabled {
-  background-color:#FFC7BB !important;
+	background-color:#FFC7BB !important;
 }
 div.ki_export table tr.even td.disabled {
-  background-color:#FFE1DB !important;
+	background-color:#FFE1DB !important;
 }
 
-
-div.ki_export table td.user
-{
+div.ki_export table td.user {
 	width:50px;
 	text-align:left;
 }
 
-#export_head td.user
-{
+#export_head td.user {
 	width:100px;
 }
 
-
-div.ki_export table td.cleared
-{
+div.ki_export table td.cleared {
 	width:20px;
 }
 
-div.ki_export table td.cleared a
-{
-width:13px;
-height:13px;
-display:block;
-float:left;
+div.ki_export table td.cleared a {
+	width:13px;
+	height:13px;
+	display:block;
+	float:left;
 }
 
-div.ki_export table td.cleared a.is_cleared
-{
-background:url('../grfx/cleared.png') 0 -13px  no-repeat;
+div.ki_export table td.cleared a.is_cleared {
+	background:url('../grfx/cleared.png') 0 -13px  no-repeat;
 }
 
-div.ki_export table td.cleared a.isnt_cleared
-{
-background:url('../grfx/cleared.png') no-repeat;
+div.ki_export table td.cleared a.isnt_cleared {
+	background:url('../grfx/cleared.png') no-repeat;
 }
 
-
-
-
-
-div#export_head div.right
-{ 
+div#export_head div.right { 
     position:absolute;
     overflow:hidden;
     width:32px;
@@ -298,19 +267,15 @@ div#export_head div.right
     right:3px;
 }
 
-div#export_head div.right a
-{ 
+div#export_head div.right a {
     background-image: url('../grfx/invert.png');
     overflow:hidden;
     display:block;
     width:22px;
     height:16px;
     text-indent:-500px;
-
 }
 
-div#export_head td {padding: 3px 4px 4px 6px;}
-
-
-
-
+div#export_head td {
+	padding: 3px 4px 4px 6px;
+}
