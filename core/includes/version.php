ec0f0fbad360b3668da8b8acd167b416fb447424
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2015-10-29 17:47:12 +0100

    [TASK] Change double quotes to single quotes
    Just for release script

--- core/includes/version.php
+++ core/includes/version.php
@@ -1,4 +1,4 @@
 <?php
-$kga['version']  = "0.9.3";             // kimai version
-$kga['revision'] = 1384;                // database revision number (incremented whenever the database changes)
-$kga['status']   = "Release Candidate"; // leave blank if stable ...
+$kga['version'] = '0.9.3';
+$kga['revision'] = 1384; // database revision number (incremented whenever the database changes)
+$kga['status'] = 'Release Candidate'; // leave blank if stable
