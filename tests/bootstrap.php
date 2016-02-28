ec681c3f1223ff8ca024a9c792248b7783b7c5de
Kevin Papst <kpapst@gmx.net>
2016-02-28 13:38:43 +0100

    working on autoloading for unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,34 +21,7 @@
 // TODO: check include path
 ini_set ( 'date.timezone', 'Europe/Berlin' );
 
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require_once __DIR__ . '/../vendor/autoload.php';
+if (!file_exists(__DIR__ . '/../libraries/autoload.php')) {
+    die('You have to execute "composer install" or "composer update" before executing unit tests!');
 }
-
-ini_set('include_path', ini_get('include_path')
-    . PATH_SEPARATOR . __DIR__ . '/library'
-    . PATH_SEPARATOR . __DIR__ . '/../core/libraries'
-);
-
-class UnitTestHelper
-{
-    /**
-     * Access protected or private methods
-     *
-     * use the following code to access any protected or private class method
-     * $obj = new MyClass();
-     * $method = UnitTestHelper::getMethod($obj, 'nameOfMethod');
-     * $result = $method->invoke($obj,'param1','param2');
-     *
-     * @param Object|string $obj
-     * @param string $name
-     *
-     * @return method
-     */
-    public static function getMethod($obj, $name) {
-        $class = new ReflectionClass($obj);
-        $method = $class->getMethod($name);
-        $method->setAccessible(true);
-        return $method;
-    }
-}
+require_once __DIR__ . '/../libraries/autoload.php';
