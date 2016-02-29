e3147e8fe905b122e401155d7592012222732b89
Kevin Papst <kpapst@gmx.net>
2016-02-29 23:54:31 +0100

    compact footer on welcome screen

--- templates/scripts/misc/copyrightnotes.php
+++ templates/scripts/misc/copyrightnotes.php
@@ -4,12 +4,7 @@
     other time tracking software, please.
 -->
 <p>
-    <strong>Kimai <span style="color:red"><?php echo $this->kga['status']?></span> <?php echo $this->kga['version'],'.',$this->kga['revision']?></strong> - &copy; <?php $this->devtimespan?> the <a href="http://www.kimai.org" target="_blank">Kimai Team</a><br />
-    This software is provided under the terms and conditions of the <a href="COPYING" target="_blank">General Public License Version 3</a><br />
-    Powered by 
-        <a href="http://www.jquery.com" target="_blank">jQuery</a> |  
-        <a href="http://framework.zend.com/" target="_blank">Zend Framework</a> |
-        <a href="http://php.net/" target="_blank">PHP</a> |  
-        <a href="http://mysql.com/" target="_blank">MySQL</a>
+    <strong>Kimai <span style="color:red"><?php echo $this->kga['status']?></span> <?php echo $this->kga['version'],'.',$this->kga['revision']?></strong> - &copy; <?php $this->devtimespan?> by <a href="http://www.kimai.org" target="_blank">Kimai Team</a><br />
+    This software is provided under the terms and conditions of the <a href="COPYING" target="_blank">GPL v3</a><br />
 </p>
 <p id="checkupdate"></p>
