7eeb01ceae32221ea3f2eee4b7e2d362bcb070cb
Kevin Papst <kpapst@gmx.net>
2016-03-13 22:01:18 +0100

    removed unused files

--- extensions/ki_invoice/templates/scripts/timespan.php
+++
@@ -1,3 +0,0 @@
-<strong>
-        <?php echo $this->escape(strftime($this->kga['date_format'][2], $this->timeframe[0])) . ' - ' .
-            $this->escape(strftime($this->kga['date_format'][2], $this->timeframe[1])) ?></strong>
