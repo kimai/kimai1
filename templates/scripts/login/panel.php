68cfa99e8dc3e27845597786af37489c2a9e5eb9
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2016-03-31 20:59:57 +0200

    Update panel.php
    * optimize cookie test
    * reformat code

--- templates/scripts/login/panel.php
+++ templates/scripts/login/panel.php
@@ -1,118 +1,114 @@
 <!DOCTYPE html>
 <head>
-    <link rel="SHORTCUT ICON" href="favicon.ico">
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-    <meta name="robots" content="noindex,nofollow" />
-    <title>Kimai <?php echo $this->translate('login') ?></title>
-    <script type="text/javascript" src="libraries/jQuery/jquery-1.9.1.min.js"></script>
-    <script type="text/javascript" src="libraries/jQuery/jquery.cookie.js"></script>
-    <script type='text/javascript'>
-    $(function(){
-        $.cookie('KimaiCookietest', 'jes');
-        KimaiCookietest = $.cookie('KimaiCookietest');
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<meta name="robots" content="noindex,nofollow" />
+	<title>Kimai <?php echo $this->translate('login') ?></title>
+	<link rel="SHORTCUT ICON" href="favicon.ico">
+	<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->skin('login.css'); ?>" />
+	<script type="text/javascript" src="libraries/jQuery/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="libraries/jQuery/jquery.cookie.js"></script>
+	<script type='text/javascript'>
+	$(function(){
+		$.cookie('KimaiCookieTest', 'yes');
 
-        if (KimaiCookietest == 'jes') {
-            $("#cookiewarning").remove();
-            $.cookie('KimaiCookietest', '', {expires: -1});
-        }
+		if ($.cookie('KimaiCookieTest') == 'yes') {
+			$("#cookiewarning").remove();
+			$.cookie('KimaiCookieTest', '', {expires: -1});
+		}
 
-        if ($("#warning").find("p").size() < 2) {
-           $("#warning").remove();
-        }
+		if ($("#warning").find("p").size() < 2) {
+			$("#warning").remove();
+		}
 
-        $("#forgotPasswordLink").click(function(event) {
-          event.preventDefault();
-          $("#login").fadeOut();
-          $("#forgotPasswordUsername").val("");
-          $("#forgotPassword").fadeIn();
-          return false;
-        });
+		$("#forgotPasswordLink").click(function(event) {
+			event.preventDefault();
+			$("#login").fadeOut();
+			$("#forgotPasswordUsername").val("");
+			$("#forgotPassword").fadeIn();
+			return false;
+		});
 
-        $("#resetPassword").click(function(event) {
-          event.preventDefault();
-          $("#forgotPasswordUsername").blur();
-          $.ajax({
-            type: "POST",
-            url: "processor.php?a=forgotPassword",
-            data: {
-              name:  $("#forgotPasswordUsername").val()
-            },
-            dataType: "json",
-            success: function(data) {
-              $("#forgotPassword").fadeOut();
-              $("#forgotPasswordConfirmation").find("p").html(data.message);
-              $("#forgotPasswordConfirmation").fadeIn();
-            }
-          });
-          return false;
-        });
+		$("#resetPassword").click(function(event) {
+			event.preventDefault();
+			$("#forgotPasswordUsername").blur();
+			$.ajax({
+				type: "POST",
+				url: "processor.php?a=forgotPassword",
+				data: {
+					name: $("#forgotPasswordUsername").val()
+				},
+				dataType: "json",
+				success: function(data) {
+					$("#forgotPassword").fadeOut();
+					$("#forgotPasswordConfirmation").find("p").html(data.message);
+					$("#forgotPasswordConfirmation").fadeIn();
+				}
+			});
+			return false;
+		});
 
-        $(".returnToLogin").click(function(event) {
-          event.preventDefault();
-          $("#login").fadeIn();
-          $("#forgotPassword").fadeOut();
-          $("#forgotPasswordConfirmation").fadeOut();
-          return false;
-        });
+		$(".returnToLogin").click(function(event) {
+			event.preventDefault();
+			$("#login").fadeIn();
+			$("#forgotPassword").fadeOut();
+			$("#forgotPasswordConfirmation").fadeOut();
+			return false;
+		});
 
-        $("#kimaiusername").focus();
-    });
-    </script>
+		$("#kimaiusername").focus();
+	});
+	</script>
 <?php if ($this->kga['check_at_startup']): ?>
-    <script type="text/javascript" src="js/main.js"></script>
-    <script type='text/javascript'>
-    $(function(){
-        checkupdate("core/");
-    });
-    </script>
+	<script type="text/javascript" src="js/main.js"></script>
+	<script type="text/javascript">
+	$(function(){
+		checkupdate("core/");
+	});
+	</script>
 <?php endif; ?>
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->skin('login.css'); ?>" />
 </head>
 <body>
 <div id="content">
-    <div id="box">
-        <div id="login" style="display:block">
-            <form action="index.php?a=checklogin" name="form1" method="post">
-                <fieldset>
-                    <label for="kimaiusername">
-                        <?php echo $this->translate('username')?>:
-                    </label>
-                    <input type="text" name="name" id="kimaiusername" />
-                    <label for="kimaipassword">
-                        <?php echo $this->translate('password')?>:
-                    </label>
-                    <input type="password" name="password" id="kimaipassword" />
-                    <button id="loginButton" type='submit'></button>
-                    <a id="forgotPasswordLink" href=""><?php echo $this->translate('forgotPassword') ?></a>
-                </fieldset>
-            </form>
-        </div>
-
-        <div id="forgotPassword">
-            <?php echo $this->translate('passwordReset:instructions'); ?>
-            <form action="">
-                <fieldset>
-                    <label for="forgotPasswordUsername">
-                        <?php echo $this->translate('username') ?>:
-                    </label>
-                    <input type="text" name="name" id="forgotPasswordUsername" />
-                    <button id="resetPassword" type="submit"><?php echo $this->translate('passwordReset:button'); ?></button>
-                </fieldset>
-            </form>
-            <a class="returnToLogin" href=""><?php echo $this->translate('passwordReset:returnToLogin') ?></a>
-        </div>
-
-        <div id="forgotPasswordConfirmation">
-            <p></p>
-            <a class="returnToLogin" href=""><?php echo $this->translate('passwordReset:returnToLogin') ?></a>
-        </div>
-            
-        <div id="warning" style="display:block">
-            <p id="JSwarning"><strong style="color:red"><?php echo $this->translate('JSwarning') ?></strong></p>
-            <p id="cookiewarning"><strong style="color:red"><?php echo $this->translate('cookiewarning') ?></strong></p>
-        </div>
-    </div>
-    <?php echo $this->partial('misc/copyrightnotes.php', array('kga' => $this->kga, 'devtimespan' => $this->devtimespan)); ?>
+	<div id="box">
+		<div id="login" style="display:block">
+			<form action="index.php?a=checklogin" name="form1" method="post">
+				<fieldset>
+					<label for="kimaiusername">
+						<?php echo $this->translate('username')?>:
+					</label>
+					<input type="text" name="name" id="kimaiusername" />
+					<label for="kimaipassword">
+						<?php echo $this->translate('password')?>:
+					</label>
+					<input type="password" name="password" id="kimaipassword" />
+					<button id="loginButton" type='submit'></button>
+					<a id="forgotPasswordLink" href=""><?php echo $this->translate('forgotPassword') ?></a>
+				</fieldset>
+			</form>
+		</div>
+		<div id="forgotPassword">
+			<?php echo $this->translate('passwordReset:instructions'); ?>
+			<form action="">
+				<fieldset>
+					<label for="forgotPasswordUsername">
+						<?php echo $this->translate('username') ?>:
+					</label>
+					<input type="text" name="name" id="forgotPasswordUsername" />
+					<button id="resetPassword" type="submit"><?php echo $this->translate('passwordReset:button'); ?></button>
+				</fieldset>
+			</form>
+			<a class="returnToLogin" href=""><?php echo $this->translate('passwordReset:returnToLogin') ?></a>
+		</div>
+		<div id="forgotPasswordConfirmation">
+			<p></p>
+			<a class="returnToLogin" href=""><?php echo $this->translate('passwordReset:returnToLogin') ?></a>
+		</div>
+		<div id="warning" style="display:block">
+			<p id="JSwarning"><strong style="color:red"><?php echo $this->translate('JSwarning') ?></strong></p>
+			<p id="cookiewarning"><strong style="color:red"><?php echo $this->translate('cookiewarning') ?></strong></p>
+		</div>
+	</div>
+	<?php echo $this->partial('misc/copyrightnotes.php', array('kga' => $this->kga, 'devtimespan' => $this->devtimespan)); ?>
 </div>
 </body>
 </html>
