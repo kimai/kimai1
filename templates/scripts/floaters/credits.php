08e530c38a89622d3073df379724720686eeb3a5
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2016-04-10 00:59:03 +0200

    Update credits.php
    Add more source links

--- templates/scripts/floaters/credits.php
+++ templates/scripts/floaters/credits.php
@@ -8,7 +8,7 @@
     <div class="floater_content" style="margin:10px">
         <h2>Kimai - Open Source Time Tracking</h2> 
         <p>
-            <?php echo 'v', $this->kga['version'],'.',$this->kga['revision'],' - &copy; ',$this->devtimespan;?> by the Kimai-Team:
+            <?php echo 'v' . $this->kga['version'] . '.' . $this->kga['revision'] . ' - &copy; ' . $this->devtimespan;?> by the Kimai-Team:
             <br/>
             Torsten Höltge, Severin Leonhardt, Kevin Papst, Simon Schaufelberger, Oleg Britvin, Martin Klemkow ...
         </p>
@@ -17,17 +17,15 @@
             <a href="http://forum.kimai.org/" target="_blank">Forum</a> |
             <a href="https://github.com/kimai/kimai" target="_blank">GitHub</a> |
             <a href="http://www.kimai.org/download/" target="_blank">Download</a> |
-            <a href="http://forum.kimai.org/index.php?board=10.0">Friendly Hacks</a>
+            <a href="http://forum.kimai.org/index.php?board=10.0" target="_blank">Friendly Hacks</a>
         </p>
         <p>
-            <strong>
-            <?php
+            <strong><?php
                 echo sprintf(
                     $this->kga['lang']['credits_license'],
-                    '<a href="../COPYING">GPL 3</a>'
+                    '<a href="../COPYING" target="_blank">GPL 3</a>'
                 );
-            ?>
-            </strong>
+            ?></strong>
         </p>
         <p>
             <?php
@@ -37,7 +35,7 @@
                 'https://github.com/kimai/kimai/archive/master.zip',
                 'http://www.kimai.org/donate/',
                 'https://github.com/kimai/kimai/issues',
-                'https://github.com/kimai/kimai/tree/master/core/language'
+                'https://github.com/kimai/kimai/tree/master/language'
             );
             ?>
         </p>
@@ -50,16 +48,22 @@
         </p>
         <p>
             <strong><?php echo $this->kga['lang']['credits_libs']?></strong>
-             <a href="http://framework.zend.com/" target="_blank">Zend Framework</a>,
-             <a href="http://www.jquery.com" target="_blank">jQuery</a>,
-             <a href="http://phpjs.org" target="_blank">phpjs</a>,
-             <a href="http://www.tinybutstrong.com/" target="_blank">TinyButStrong</a>,
-             <a href="http://www.tcpdf.org/" target="_blank">TCPDF</a>,
-             <a href="http://www.phpclasses.org/ultimatemysql" target="_blank">Ultimate MySQL Class</a>,
-             <a href="http://www.phpconcept.net" rel="nofollow" target="_blank">PclZip</a>,
-             <a href="https://getcomposer.org/" target="_blank">Composer</a>,
-             <a href="http://mysql.com/" target="_blank">MySQL</a>,
-             <a href="http://php.net/" target="_blank">PHP</a>
+                <a href="http://framework.zend.com/" target="_blank">Zend Framework</a>,
+                <a href="http://jquery.com/" target="_blank">jQuery</a>,
+                <a href="http://phpjs.org/" target="_blank">phpjs</a>,
+                <a href="https://github.com/js-cookie/js-cookie" target="_blank">js-cookie</a>,
+                <a href="https://github.com/malsup/form" target="_blank">jQuery Form Plugin</a>,
+                <a href="https://github.com/SamWM/jQuery-Plugins/tree/master/newsticker" target="_blank">jQuery newsticker</a>,
+                <a href="https://github.com/SamWM/jQuery-Plugins/tree/master/selectboxes" target="_blank">jQuery selectboxes</a>,
+                <a href="http://jqplot.com" target="_blank">jqPlot</a>,
+                <a href="http://www.tinybutstrong.com/" target="_blank">TinyButStrong</a>,
+                <a href="http://tinydoc.unesolution.fr/" target="_blank">tinyDoc</a>,
+                <a href="http://www.tcpdf.org/" target="_blank">TCPDF</a>,
+                <a href="http://www.phpclasses.org/ultimatemysql" target="_blank">Ultimate MySQL Class</a>,
+                <a href="http://www.phpconcept.net/" target="_blank">PclZip</a>,
+                <a href="https://getcomposer.org/" target="_blank">Composer</a>,
+                <a href="http://mysql.com/" target="_blank">MySQL</a>,
+                <a href="http://php.net/" target="_blank">PHP</a>
         </p>
     </div>
 </div>
