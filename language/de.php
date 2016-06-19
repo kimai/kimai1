fa8ec79bdc83c6956ea5001b96792a3c820d686a
Simon Schaufelberger <simonschaufi@googlemail.com>
2016-06-19 23:23:32 +0200

    Bug: Check if user exists
    When creating a new user, a check is missing if the user exists already.
    
    Resolves: #701

--- language/de.php
+++ language/de.php
@@ -338,6 +338,7 @@ EOD
     'errorMessages' => array(
         'permissionDenied' => 'Keine Berechtigung',
         'userWithSameName' => 'Kundennamen dürfen nicht gleich lauten wie Benutzernamen.',
+        'userExistsAlready' => 'Benutzer existiert bereits.',
         'customerWithSameName' => 'Benutzernamen dürfen nicht gleich lauten wie Kundennamen.',
         'sameGlobalRoleName' => 'Eine globale Rolle mit diesem Namen existiert bereits.',
         'sameMembershipRoleName' => 'Eine gruppenbezogene Rolle mit diesem Namen existiert bereits.',
