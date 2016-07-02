1772752ddde82f6b30b652a5fc7d77851d25efa9
Simon Schaufelberger <simonschaufi@googlemail.com>
2016-07-03 01:02:56 +0200

    :sparkles: 1.0.0 Release

--- includes/version.php
+++ includes/version.php
@@ -1,4 +1,4 @@
 <?php
-$kga['version'] = '0.9.3';
+$kga['version'] = '1.0.0';
 $kga['revision'] = 1387; // database revision number (incremented whenever the database changes)
-$kga['status'] = 'dev'; // leave blank if stable
+$kga['status'] = ''; // leave blank if stable
