71c0ae76b82069d6e5f31498be43653c407f5df1
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2016-07-03 02:05:38 +0200

    Update version.php

--- includes/version.php
+++ includes/version.php
@@ -1,4 +1,4 @@
 <?php
 $kga['version'] = '1.0.0';
 $kga['revision'] = 1387; // database revision number (incremented whenever the database changes)
-$kga['status'] = ''; // leave blank if stable
+$kga['status'] = 'dev'; // leave blank if stable
