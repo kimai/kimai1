b44a6c3530e21c2355898718959b27bb8608410e
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2016-08-20 23:15:53 +0200

    Forgot to increase revision number

--- includes/version.php
+++ includes/version.php
@@ -1,4 +1,4 @@
 <?php
 $kga['version'] = '1.0.1';
-$kga['revision'] = 1388; // database revision number (incremented whenever the database changes)
+$kga['revision'] = 1389; // database revision number (incremented whenever the database changes)
 $kga['status'] = 'dev'; // leave blank if stable
