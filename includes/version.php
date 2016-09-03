c08af5e1654f65d62e105ce9d71675e549d60ecd
Simon Schaufelberger <simonschaufi@googlemail.com>
2016-09-03 17:43:52 +0200

    [BUGFIX] increase revision and fix installer

--- includes/version.php
+++ includes/version.php
@@ -1,4 +1,4 @@
 <?php
-$kga['version'] = '1.2.0';
-$kga['revision'] = 1391; // database revision number (incremented whenever the database changes)
+$kga['version'] = '1.2.1';
+$kga['revision'] = 1392; // database revision number (incremented whenever the database changes)
 $kga['status'] = 'dev'; // leave blank if stable
