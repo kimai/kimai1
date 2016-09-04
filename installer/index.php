da9b214cbe2b34419e513b999dc3c4ca09a1f6e1
Simon Schaufelberger <simonschaufi@googlemail.com>
2016-09-04 02:34:19 +0200

    [BUGFIX] Fix broken installer

--- installer/index.php
+++ installer/index.php
@@ -20,55 +20,52 @@
 $installsteps = 8;
 $kga = array();
 require("../includes/version.php");
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
+?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <link rel="stylesheet" href="styles.css" type="text/css" media="screen" title="no title" charset="utf-8">
-    <script src="../libraries/jQuery/jquery-1.9.1.min.js" type="text/javascript" charset="utf-8"></script>
-    <script src="installscript.js" type="text/javascript" charset="utf-8"></script>
+    <link type="text/css" rel="stylesheet" href="styles.css">
+    <script type="text/javascript" src="../libraries/jQuery/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="installscript.js"></script>
     <title>Kimai Installation</title>
     
-    <script type="text/javascript" charset="utf-8">
-        var step         = 1;
-        var back         = "";
-        var new_database = -1;
-        var language     = "";
-        var database     = "";
-        var prefix       = "";
-        var hostname     = "";
-        var username     = "";
-        var password     = "";
-        var timezone     = "";
+    <script type="text/javascript">
+        var step = 1;
+        var current = 0;
+        var back = '';
+        var target = '';
+        var new_database = false;
+        var language = '';
+        var database = '';
+        var prefix = '';
+        var hostname = '';
+        var username = '';
+        var password = '';
+        var timezone = '';
     </script>
 </head>
-
 <body>
     <div id="wrapper" class="invisible">
         <div id="header">
-        <div id="progressbar">
-        <?php
-            for ($i = 0; $i < $installsteps; $i++) {
-                echo "<span class=\"step_nope\">&nbsp;</span>";
-            }
-            $width = $i * 15;
-            echo"<script type=\"text/javascript\" charset=\"utf-8\">
-                $('#progressbar').css('width','${width}px');
-            </script>";
-        ?>
-        </div>
-        <h1>Installation <?php echo "v" . $kga['version'] . "." . $kga['revision'] ?></h1>
+            <div id="progressbar">
+            <?php
+                for ($i = 0; $i < $installsteps; $i++) {
+                    echo "<span class=\"step_nope\">&nbsp;</span>";
+                }
+                $width = $i * 15;
+                echo "<script type=\"text/javascript\">
+                    $('#progressbar').css('width','${width}px');
+                </script>";
+            ?>
+            </div>
+            <h1>Installation <?php echo "v" . $kga['version'] . "." . $kga['revision'] ?></h1>
         </div>
         <div id="body">
-            
             <div id="jswarn">
                 JavaScript MUST be activated!<br/>
                 JavaScript MUSS aktiviert sein!
             </div>
-            
-            <div class = "invisible" id = "installsteps">
+            <div class="invisible" id="installsteps">
                 <?php include 'steps/10_language.php'; ?>
             </div>      
         </div>
