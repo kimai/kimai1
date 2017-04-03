f8df14c8f3e534cc9dddb4b4ff4c4389b5031ba5
Timothy Legge <timlegge@gmail.com>
2017-04-02 23:32:55 -0300

    SMTP fix up some installer issues

--- installer/steps/30_enter_mail_server_details.php
+++ installer/steps/30_enter_mail_server_details.php
@@ -4,33 +4,31 @@ require "../../includes/func.php";
 
 $smtp_transport = isset($_REQUEST['smtp_transport']) ? $_REQUEST['smtp_transport'] : "sendmail";
 $smtp_name = isset($_REQUEST['smtp_name']) ? $_REQUEST['smtp_name'] : "smtp.example.org";
-$smtp_host = isset($_REQUEST['smtp_host']) ? $_REQUEST['smtp_host'] : "localhost";
-$smtp_port = isset($_REQUEST['smtp_port']) ? $_REQUEST['smtp_port'] : "465";
+$smtp_host = isset($_REQUEST['smtp_host']) ? $_REQUEST['smtp_host'] : "127.0.0.1";
+$smtp_port = isset($_REQUEST['smtp_port']) ? $_REQUEST['smtp_port'] : "25";
 $smtp_user = isset($_REQUEST['smtp_user']) ? $_REQUEST['smtp_user'] : "";
 $smtp_pass = isset($_REQUEST['smtp_pass']) ? $_REQUEST['smtp_pass'] : "";
-$smtp_ssl = isset($_REQUEST['smtp_ssl']) ? $_REQUEST['smtp_ssl'] : "ssl";
+$smtp_ssl = isset($_REQUEST['smtp_ssl']) ? $_REQUEST['smtp_ssl'] : "";
 $smtp_auth = isset($_REQUEST['smtp_auth']) ? $_REQUEST['smtp_auth'] : "login";
 
-function get_smtp_transport_list() {
+function get_list($id, $selected, $list) {
     $html = '';
-    global $smtp_transport;
-    $trans = array('sendmail', 'smtp', 'file');
-    foreach ($trans as $t) {
-        if ($t == $smtp_transport) {
-            $html .= '<option selected="selected">' . $t . '</option>';
+
+    foreach ($list as $item) {
+        if ($item == $selected) {
+            $html .= '<option selected="selected">' . $item . '</option>';
         }
         else {
-            $html .= '<option>'.$t.'</option>';
+            $html .= '<option>'.$item.'</option>';
         }
     }
 
     if (!empty($html)) {
-        return '<select name="smtp_transport" id="smtp_transport">' . $html . '</select>';
+//        return '<select name="' . $id . '" id="' . $id .'" >' . $html . '</select>';
+        return '<select name="' . $id . '" id="' . $id .'" onchange="mail_transport_select();" >' . $html . '</select>';
     }
 
-    // sometimes fetching the list of timezones seems to fail
-    // see https://github.com/kimai/kimai/issues/579
-    return '<input type="text" value="' . $serverZone . '" id="timezone">';
+    return '<input type="text" value="' . $selected . '" id="timezone">';
 }
 
 if ($_REQUEST['lang'] == "en") {
@@ -41,30 +39,33 @@ if ($_REQUEST['lang'] == "en") {
     
     <table border="0" cellspacing="0" cellpadding="5">
         <tr>
-            <td>Mail Transport Type:<br/> <?php echo get_smtp_transport_list(); ?>
+            <td>Mail Transport Type:<br/> <?php echo get_list("smtp_transport", $smtp_transport, array('sendmail', 'smtp', 'file')); ?>
             </td>
         </tr>
-        <tr>
-            <td>Description:<br/><input id="smtp_name" type="text" value="<?php echo $smtp_name; ?>"/>   </td>
+        <tr class="smtp">
+            <td>Name:<br/><input id="smtp_name" type="text" value="<?php echo $smtp_name; ?>"/>   </td>
         </tr>
-        <tr>
+        <tr class="smtp">
             <td>Mail Server:<br/><input id="smtp_host" type="text" value="<?php echo $smtp_host; ?>"/>   </td>
             <td>Port:<br/><input id="smtp_port" type="text" value="<?php echo $smtp_port; ?>"/>   </td>
         </tr>
-        <tr>
-            <td>Auth Method:<br/><input id="smtp_auth" type="text" value="<?php echo $smtp_auth; ?>"/></td>
-        </tr>
-        <tr>
+        <tr class="smtp">
+            <td>Auth Method:<br/> <?php echo get_list("smtp_auth", $smtp_auth, array('login', 'plain', 'crammd5')); ?>
+            </td>
+        </tr class="smtp">
+        <tr class="smtp">
             <td>User:<br/><input id="smtp_user" type="text" value="<?php echo $smtp_user; ?>"/>   </td>
             <td>Password:<br/><input id="smtp_pass" type="password" value="<?php echo $smtp_pass; ?>"/>   </td>
         </tr>
-        <tr>
-            <td>SSL:<br/><input id="smtp_ssl" type="text" value="<?php echo $smtp_ssl; ?>"/>   </td>
+        <tr class="smtp">
+            <td>SSL:<br/> <?php echo get_list("smtp_ssl", $smtp_ssl, array('', 'ssl', 'tls')); ?>
+            </td>
         </tr>
     </table>
     <span id='caution'></span><br />
     <button onclick="step_back(); return false;">Back</button>
     <button onclick="mail_proceed(); return false;" class="proceed">Proceed</button>
+
     <?php
 } else {
     ?>
@@ -74,24 +75,27 @@ if ($_REQUEST['lang'] == "en") {
 
     <table border="0" cellspacing="0" cellpadding="5">
         <tr>
-            <td>Transportart:<br/><input id="smtp_transport" type="text" value="$smtp_transport"/>   </td>
+            <td>Transportart:<br/><?php echo get_list("smtp_transport", $smtp_transport, array('sendmail', 'smtp', 'file')); ?>
+            </td>
         </tr>
-        <tr>
-            <td>Beschreibung:<br/><input id="smtp_name" type="text" value="$smtp_name"/>   </td>
+        <tr class="smtp">
+            <td>Beschreibung:<br/><input id="smtp_name" type="text" value="<?php echo $smtp_name; ?>"/>   </td>
         </tr>
-        <tr>
-            <td>Host:<br/><input id="smtp_host" type="text" value="$smtp_host"/>   </td>
-            <td>TCP-Port:<br/><input id="smtp_port" type="text" value="$smtp_port"/>   </td>
+        <tr class="smtp">
+            <td>Host:<br/><input id="smtp_host" type="text" value="<?php echo $smtp_host; ?>"/>   </td>
+            <td>TCP-Port:<br/><input id="smtp_port" type="text" value="<?php echo $smtp_port; ?>"/>   </td>
         </tr>
-        <tr>
-            <td>Authentifizierung:<br/><input id="smtp_auth" type="text" value="$smtp_auth"/></td>
+        <tr class="smtp">
+            <td>Authentifizierung:<br/> <?php echo get_list("smtp_auth", $smtp_auth, array('login', 'plain', 'crammd5')); ?>
+            </td>
         </tr>
-        <tr>
-            <td>Benutzer:<br/><input id="smtp_user" type="text" value="$smtp_user"/>   </td>
-            <td>Passwort:<br/><input id="smtp_pass" type="password" value="$smtp_pass"/>   </td>
+        <tr class="smtp">
+            <td>Benutzer:<br/><input id="smtp_user" type="text" value="<?php echo $smtp_user; ?>"/>   </td>
+            <td>Passwort:<br/><input id="smtp_pass" type="password" value="<?php echo $smtp_pass; ?>"/>   </td>
         </tr>
-        <tr>
-            <td>SSL:<br/><input id="smtp_ssl" type="text" value="$smtp_ssl"/>   </td>
+        <tr class="smtp">
+            <td>SSL:<br/> <?php echo get_list("smtp_ssl", $smtp_ssl, array('', 'ssl', 'tls')); ?>
+            </td>
         </tr>
     </table>
     <span id='caution'></span><br /><br />
