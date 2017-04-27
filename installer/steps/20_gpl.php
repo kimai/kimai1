6221f163ca44df8bd8388c59def51796e3021d1f
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2017-04-27 17:19:48 +0200

    Update 20_gpl.php

--- installer/steps/20_gpl.php
+++ installer/steps/20_gpl.php
@@ -1,20 +1,6 @@
 <?php
 
-if ($_REQUEST['lang'] == "en") {
-    echo <<<EOD
-        <h2>License</h2>
-        <div class="gpl">
-EOD;
-    require("20_gpl_en.php");
-    echo <<<EOD
-        </div>
-        <input type="checkbox" name="accept" value='1' onclick="gpl_agreed(this);" style="width:15px;height:15px;display:inline;">
-        <span style="margin-top:5px">I agree to the terms of the General Public License.</span><br/>
-        <!--<button onclick="step_back(); return false;" class="">Back</button>-->
-        <button onclick="gpl_proceed(); return false;" class="invisible proceed">Proceed</button>
-EOD;
-
-} elseif ($_REQUEST['lang'] == "de") {
+if ($_REQUEST['lang'] == "de") {
     echo <<<EOD
         <h2>Lizenz</h2>
         <div class="gpl">
@@ -40,6 +26,20 @@ EOD;
         <span style="margin-top:5px">Съгласен съм с условията на General Public License.</span><br/>
         <!--<button onclick="step_back(); return false;" class="">Назад</button>-->
         <button onclick="gpl_proceed(); return false;" class="invisible proceed">Напред</button>
+EOD;
+
+} else {
+    echo <<<EOD
+        <h2>License</h2>
+        <div class="gpl">
+EOD;
+    require("20_gpl_en.php");
+    echo <<<EOD
+        </div>
+        <input type="checkbox" name="accept" value='1' onclick="gpl_agreed(this);" style="width:15px;height:15px;display:inline;">
+        <span style="margin-top:5px">I agree to the terms of the General Public License.</span><br/>
+        <!--<button onclick="step_back(); return false;" class="">Back</button>-->
+        <button onclick="gpl_proceed(); return false;" class="invisible proceed">Proceed</button>
 EOD;
 
 }
