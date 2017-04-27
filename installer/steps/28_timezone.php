3973fc6625f5d3a04e4959a1329f9777492cf56e
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2017-04-27 17:23:16 +0200

    Update 28_timezone.php

--- installer/steps/28_timezone.php
+++ installer/steps/28_timezone.php
@@ -36,24 +36,12 @@ function getTimezoneInputField()
     return '<input type="text" value="' . $serverZone . '" id="timezone">';
 }
 
-if ($_REQUEST['lang'] == "en") {
-    ?>
-    <h2>Timezone</h2>
-    Select your timezone. This will be used as the default for new users and customers.<br/>
-    Users can change their timezone through their preferences, customers can't. This setting can be changed using the
-    <i>Admin Panel</i>.
-    <br/><br/>
-    <?php echo getTimezoneInputField(); ?>
-    <br/><br/>
-    <button onclick="step_back(); return false;">Back</button>
-    <button onclick="timezone_proceed(); return false;" class="proceed">Proceed</button>
-    <?php
-} elseif ($_REQUEST['lang'] == "de") {
+if ($_REQUEST['lang'] == "de") {
     ?>
     <h2>Zeitzone</h2>
     Wählen Sie ihre Zeitzone aus. Diese wird als Standard für neue Benutzer und Kunden verwendet.<br/>
     Benutzer können später ihre eigene Zeitzone auswählen, Kunden jedoch nicht. Die Einstellung kann später im <i>Admin
-        Panel</i> geändert werden.
+    Panel</i> geändert werden.
     <br/><br/>
     <?php echo getTimezoneInputField(); ?>
     <br/><br/>
@@ -71,5 +59,17 @@ if ($_REQUEST['lang'] == "en") {
     <br/><br/>
     <button onclick="step_back(); return false;">Назад</button>
     <button onclick="timezone_proceed(); return false;" class="proceed">Напред</button>
+    <?php
+} else {
+    ?>
+    <h2>Timezone</h2>
+    Select your timezone. This will be used as the default for new users and customers.<br/>
+    Users can change their timezone through their preferences, customers can't. This setting can be changed using the
+    <i>Admin Panel</i>.
+    <br/><br/>
+    <?php echo getTimezoneInputField(); ?>
+    <br/><br/>
+    <button onclick="step_back(); return false;">Back</button>
+    <button onclick="timezone_proceed(); return false;" class="proceed">Proceed</button>
     <?php
 }
