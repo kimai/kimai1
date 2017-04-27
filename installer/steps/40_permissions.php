706ccd043960b242f996aa29d388b743e8959877
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2017-04-27 17:24:04 +0200

    Update 40_permissions.php

--- installer/steps/40_permissions.php
+++ installer/steps/40_permissions.php
@@ -1,20 +1,7 @@
 <?php 
 echo '<script type="text/javascript" charset="utf-8">current=40;</script>';
 
-if ($_REQUEST['lang'] == "en") {
-    echo<<<EOD
-    <h2>Write-permissions</h2>
-    PHP must have permissions to write to the following files/folders:<br/><br/>
-
-    <span class="ch_temporary"><strong>/temporary</strong> <span class="gray">(Directory)</span></span><br/>
-    <span class="ch_logfile"><strong>/temporary/logfile.txt</strong> <span class="gray">(File)</span></span><br/>
-    <span class="ch_autoconf"><strong>/includes/autoconf.php</strong> <span class="gray">(File)</span> and <strong>/includes</strong> <span class="gray">(Directory)</span></span><br/><br/>
-    <button onclick="step_back(); return false;" class="">Back</button>
-    <button class="cp-button" onclick='check_permissions();'>Check permissions now</button>
-    <button onclick="cp_proceed(); return false;" class="invisible proceed">Proceed</button>
-    <span class="ch_correctit invisible arrow">Please correct the write-permissions and press the button again!</span><br/>
-EOD;
-} elseif ($_REQUEST['lang'] == "de") {
+if ($_REQUEST['lang'] == "de") {
     echo<<<EOD
     <h2>Schreibrechte</h2>
     PHP muss Schreibrechte auf folgende Dateien und Ordner haben:<br/><br/>
@@ -39,6 +26,19 @@ EOD;
     <button class="cp-button" onclick='check_permissions();'>Провери достъпа</button>
     <button onclick="cp_proceed(); return false;" class="invisible proceed">Напред</button>
     <span class="ch_correctit invisible arrow">Поправете достъпа и натиснете бутона отново!</span><br/>
+EOD;
+} else {
+    echo<<<EOD
+    <h2>Write-permissions</h2>
+    PHP must have permissions to write to the following files/folders:<br/><br/>
+
+    <span class="ch_temporary"><strong>/temporary</strong> <span class="gray">(Directory)</span></span><br/>
+    <span class="ch_logfile"><strong>/temporary/logfile.txt</strong> <span class="gray">(File)</span></span><br/>
+    <span class="ch_autoconf"><strong>/includes/autoconf.php</strong> <span class="gray">(File)</span> and <strong>/includes</strong> <span class="gray">(Directory)</span></span><br/><br/>
+    <button onclick="step_back(); return false;" class="">Back</button>
+    <button class="cp-button" onclick='check_permissions();'>Check permissions now</button>
+    <button onclick="cp_proceed(); return false;" class="invisible proceed">Proceed</button>
+    <span class="ch_correctit invisible arrow">Please correct the write-permissions and press the button again!</span><br/>
 EOD;
 }
 echo '<script type="text/javascript" charset="utf-8">check_permissions();</script>';
