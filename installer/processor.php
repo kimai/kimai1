617132e0fb391bbf8f652fa6c792611a6e3a4fbd
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2017-08-12 12:38:44 +0200

    Bugfix: Unable to install
    FIXES #1001, #930
    
    Changes proposed in this pull request:
    - ini_get('memory_limit') returns something like 500M. This value needs to be an integer for further processing
    
    Reason for this pull request:
    - Installation is not possible on some servers

--- installer/processor.php
+++ installer/processor.php
@@ -31,6 +31,9 @@ function return_bytes($val)
 {
     $val = trim($val);
     $last = strtolower($val[strlen($val) - 1]);
+    if ($last) {
+        $val = substr($val, 0, strlen($val) - 1);
+    }
     switch ($last) {
         // The 'G' modifier is available since PHP 5.1.0
         case 'g':
