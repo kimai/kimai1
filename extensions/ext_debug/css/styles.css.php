da4331f84dce564b3c3493b096c9bf8b51b1d54b
Philipp Doblhofer <p.doblhofer@codeaware.at>
2018-02-20 12:17:19 +0100

    Standard Skin: Remove unused images, cleanup

--- extensions/ext_debug/css/styles.css.php
+++ extensions/ext_debug/css/styles.css.php
@@ -1,6 +1,5 @@
 <?php
 	header('Content-type: text/css');
-	$filter_bg = "../../../skins/standard/grfx/filter_bg.jpg";
 ?>
 
 #deb_ext_kga_wrap {
@@ -70,7 +69,14 @@
 }
     
 #deb_ext_shoutbox input {
-    background-image:url('<?php echo $filter_bg; ?>');
+    background: -moz-linear-gradient(270deg, rgba(136,136,136,1) 0%, rgba(96,96,96,1) 100%); /* ff3.6+ */
+	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, rgba(136,136,136,1)), color-stop(100%, rgba(96,96,96,1))); /* safari4+,chrome */
+	background: -webkit-linear-gradient(270deg, rgba(136,136,136,1) 0%, rgba(96,96,96,1) 100%); /* safari5.1+,chrome10+ */
+	background: -o-linear-gradient(270deg, rgba(136,136,136,1) 0%, rgba(96,96,96,1) 100%); /* opera 11.10+ */
+	background: -ms-linear-gradient(270deg, rgba(136,136,136,1) 0%, rgba(96,96,96,1) 100%); /* ie10+ */
+	background: linear-gradient(180deg, rgba(136,136,136,1) 0%, rgba(96,96,96,1) 100%); /* w3c */
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#888888', endColorstr='#606060',GradientType=0 ); /* ie6-9 */
+
     width:150px;
     border:1px solid #555;
     height:12px;
