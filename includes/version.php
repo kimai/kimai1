007809838f3b1f6ba3a70fd155f0f78588a6fe29
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2018-05-19 14:39:40 +0200

    Release 1.3.1

--- includes/version.php
+++ includes/version.php
@@ -1,4 +1,4 @@
 <?php
-$kga['version'] = '1.3.0';
+$kga['version'] = '1.3.1';
 $kga['revision'] = 1394; // database revision number (incremented whenever the database changes)
 $kga['status'] = ''; // leave blank if stable
